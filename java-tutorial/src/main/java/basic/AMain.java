3f3b68693b8537a420b3da7f73bd23c6e26999e0
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2019-09-12 19:30:21 +0700

    #

--- java-tutorial/src/main/java/basic/AMain.java
+++ java-tutorial/src/main/java/basic/AMain.java
@@ -1,9 +1,17 @@
 package basic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class AMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 
+		URL url = new URL("https://www.facebook.com/bathudaide");
+		System.out.println("PATH = " + url.getPath());
+		System.out.println("HOST = " + url.getHost());
+		System.out.println("PROTOCOL = " + url.getProtocol());
+		System.out.println("PORT = " + url.getPort());
 
 	}
 
