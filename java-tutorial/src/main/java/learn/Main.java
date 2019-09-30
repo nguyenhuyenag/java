281e240985e500b7997296bfc8ee82e1386fe806
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-09-30 17:48:12 +0700

    #

--- java-tutorial/src/main/java/learn/Main.java
+++ java-tutorial/src/main/java/learn/Main.java
@@ -1,9 +1,5 @@
 package learn;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
 
 	public static String toBinary(int n) {
@@ -16,19 +12,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		int startIndex = 2;
-		int endIndex = 4;
-		List<Integer> list = new ArrayList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(4);
-		list.add(5);
-		list.add(6);
-		List<Integer> toReverse = list.subList(startIndex, endIndex + 1);
-		Collections.reverse(toReverse);
-		list.removeAll(toReverse);
-		list.addAll(startIndex, toReverse);
+
 	}
 
 }
--- java-tutorial/src/main/java/learn/basic/exception/ConcurrentModification2.java
+++ java-tutorial/src/main/java/learn/basic/exception/ConcurrentModification2.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ConcurrentModification2 {
-	
 
 	public static void main(String[] args) {
 		int startIndex = 2;
@@ -15,12 +13,8 @@ public class ConcurrentModification2 {
 		List<Integer> list = new ArrayList<>();
 		list.addAll(Arrays.asList(1, 2, 3, 4, 5, 6));
 		// List<Integer> toReverse = list.subList(startIndex, endIndex + 1); // error
-		// List<Integer> toReverse = new ArrayList<>(list.subList(startIndex,
-		// endIndex+1));
-		List<Integer> toReverse = list.stream() //
-				.skip(startIndex) //
-				.limit(endIndex + 1) //
-				.collect(Collectors.toList());
+		List<Integer> toReverse = new ArrayList<>(list.subList(startIndex, endIndex + 1));
+		// List<Integer> toReverse = list.stream().skip(startIndex).limit(endIndex + 1).collect(Collectors.toList());
 		Collections.reverse(toReverse);
 		list.removeAll(toReverse);
 		list.addAll(startIndex, toReverse);
