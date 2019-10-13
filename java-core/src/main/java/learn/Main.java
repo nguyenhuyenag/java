23705ef42e6576b110c64cd9050cc8a5bd17de44
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2019-10-13 12:13:16 +0700

    #

--- java-core/src/main/java/learn/Main.java
+++ java-core/src/main/java/learn/Main.java
@@ -2,8 +2,16 @@ package learn;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void check(int n) {
+		while (n != 0) {
+			System.out.println(n % 10);
+			n /= 10;
+		}
+	}
 
+	public static void main(String[] args) {
+		int n = 123456;
+		check(n);
 	}
 
 }
--- java-core/src/main/java/learn/algorithm/recursion/KhuDeQuy.java
+++ java-core/src/main/java/learn/algorithm/recursion/KhuDeQuy.java
@@ -2,41 +2,4 @@ package learn.algorithm.recursion;
 
 public class KhuDeQuy {
 
-	/**
-	 * @see learn.algorithm.recursion.Recursions#digitSum
-	 */
-	public static String toBinary(int n) {
-		String str = "";
-		while (n != 0) {
-			str = n % 2 + str;
-			n = n / 2;
-		}
-		return str;
-	}
-
-	public static int sumDigit(int n) {
-		int sum = 0;
-		while (n != 0) {
-			sum += n % 10;
-			n /= 10;
-		}
-		return sum;
-	}
-
-	public static int firstDigit(int n) {
-		while (n > 10) {
-			n /= 10;
-		}
-		return n;
-	}
-
-	public static int reverseNumber(int n) {
-		int rev = 0;
-		while (n > 0) {
-			rev = rev * 10 + n % 10;
-			n = n / 10;
-		}
-		return rev;
-	}
-
 }
