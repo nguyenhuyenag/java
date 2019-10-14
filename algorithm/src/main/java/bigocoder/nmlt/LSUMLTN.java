c3abf6a39c58fca6deff3ec1fc5610354ec000a9
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-10-14 15:21:52 +0700

    #

---
+++ algorithm/src/main/java/bigocoder/nmlt/LSUMLTN.java
@@ -0,0 +1,23 @@
+package bigocoder.nmlt;
+
+import java.util.Scanner;
+
+public class LSUMLTN {
+	public static void main(String[] args) {
+		try (Scanner io = new Scanner(System.in)) {
+			int S = 1;
+			int n = io.nextInt();
+			for (int i = 2; i < n; i++) {
+				S += i;
+				// if (S > n) {// NMLT_LOOP07_SSUMGTN
+				// System.out.println(i);
+				// return;
+				// }
+				if (S >= n) {
+					System.out.println(i - 1);
+					return;
+				}
+			}
+		}
+	}
+}
