498f393bc94f15e994701ff88465eac2964c2ee6
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-10-15 14:24:24 +0700

    #

---
+++ algorithm/src/main/java/bigocoder/nmlt/CountChar.java
@@ -0,0 +1,22 @@
+package bigocoder.nmlt;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class CountChar {
+	public static void main(String[] args) {
+		try (Scanner io = new Scanner(System.in)) {
+			String str = io.nextLine();
+			char[] arr = str.toCharArray();
+			LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+			for (char key : arr) {
+				if (!map.containsKey(key)) {
+					map.put(key, 1);
+				} else {
+					map.put(key, map.get(key) + 1);
+				}
+			}
+			map.forEach((k, v) -> System.out.println(k + ": " + v));
+		}
+	}
+}
