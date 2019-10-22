528900ae17c1918f6aa766312f27f7c13ff7dfe5
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-10-22 17:40:46 +0700

    #

--- java-core/src/main/java/learn/basic/lombok/ValAndVar.java
+++ java-core/src/main/java/learn/basic/lombok/ValAndVar.java
@@ -3,12 +3,12 @@ package learn.basic.lombok;
 import lombok.val;
 import lombok.var;
 
-public class Val {
+public class ValAndVar {
 
 	public static void main(String[] args) {
 
 		val s = "1234"; // final String s = "1234"
-		// s += "5"; // The final local variable s cannot be assigned
+		// s += "5"; 	// error
 		System.out.println(s);
 
 		var str = "1234"; // String s = "1234"
