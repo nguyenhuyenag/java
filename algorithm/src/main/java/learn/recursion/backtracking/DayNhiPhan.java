e21b0275dae30782cdb94e5efc877087263b6c59
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-10-24 11:51:35 +0700

    #

--- algorithm/src/main/java/learn/recursion/backtracking/DayNhiPhan.java
+++ algorithm/src/main/java/learn/recursion/backtracking/DayNhiPhan.java
@@ -1,39 +1,40 @@
 package learn.recursion.backtracking;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Sinh các dãy nhị phân có độ dài n
  */
 public class DayNhiPhan {
 
-	static int n;
-	static int[] arr;
-	static final int[] binary = { 0, 1 };
+	private static List<String> list = new ArrayList<>();
 
-	static void output() {
-		for (int i = 0; i < n; i++) {
-			System.out.print(arr[i]);
-		}
-		System.out.println();
-	}
-
-	static void find(int i) {
-		for (int k : binary) {
-			arr[i] = k;
-			if (i == n - 1) { // nếu tìm đến i cuối cùng thì in ra kết quả hiện tại
-				output();
-			} else { // chưa tìm đến i cuối thì tăng i
-				find(i + 1);
+	private static void quayLui(int[] arr, int n, int i) {
+		for (int j = 0; j <= 1; j++) { // j = {0, 1}
+			arr[i] = j;
+			if (i == (n - 1)) {
+				StringBuilder builder = new StringBuilder();
+				for (int var : arr) {
+					builder.append(var);
+				}
+				list.add(builder.toString());
+			} else {
+				quayLui(arr, n, i + 1);
 			}
 		}
 	}
 
+	public static List<String> binarySeq(int n) {
+		int[] arr = new int[n];
+		quayLui(arr, n, 0);
+		return list; // 2^n day
+	}
+
 	public static void main(String[] args) {
-		try (Scanner io = new Scanner(System.in)) {
-			n = 2; // io.nextInt();
-			arr = new int[n];
-			find(0);
+		// binarySeq(0).forEach(System.out::println);
+		for (int i = 0; i < Math.pow(2, 4); i++) {
+			System.out.println(Integer.toBinaryString(i));
 		}
 	}
 
