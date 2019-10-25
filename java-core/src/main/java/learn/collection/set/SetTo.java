dbd23b028ce7e291b1a0505fd542e2337fcb7321
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-10-25 11:15:46 +0700

    #

--- java-core/src/main/java/learn/collection/set/SetTo.java
+++ java-core/src/main/java/learn/collection/set/SetTo.java
@@ -1,19 +1,22 @@
 package learn.collection.set;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+@SuppressWarnings("unused")
 public class SetTo {
 
-	Set<Integer> set = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5));
+	static Set<String> set = new HashSet<>(Arrays.asList("a", "b", "c", "d"));
 
 	public static void toArray() {
-
+		String[] arr = set.stream().toArray(String[]::new);
 	}
 
 	public static void toList() {
-
+		List<String> targetList = new ArrayList<>(set);
 	}
 
 }
