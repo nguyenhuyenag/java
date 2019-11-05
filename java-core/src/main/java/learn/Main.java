5ee5b2edd2a9e5a8dc3ba461b9a8e6803294df2a
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-11-05 17:44:36 +0700

    #

--- java-core/src/main/java/learn/Main.java
+++ java-core/src/main/java/learn/Main.java
@@ -1,23 +1,20 @@
 package learn;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner io = new Scanner(System.in);
-		int n = io.nextInt();
-		io.nextLine();
-		List<String> list = new ArrayList<>();
-		for (int i = 1; i <= n; i++) {
-			String str = io.nextLine();
-			String join = String.join("", list) + str;
-			list.add(join);
+	public static void main(String[] args) throws IOException {
+		int[] arr = { 1, 0, 4, 6, 17, 9, 7 };
+		int len = arr.length;
+		int sum = Arrays.stream(arr).sum();
+		float[] fArr = new float[len];
+		for (int i = 0; i < len; i++) {
+			float a = (float) arr[i] / sum;
+			fArr[i] = a;
 		}
-		System.out.println(Arrays.toString(list.toArray()));
+		System.out.println(Arrays.toString(fArr));
 	}
 
 }
