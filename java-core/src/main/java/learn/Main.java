29a4f12945d9db1c4e81e7b7cd6da52da0042b5c
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-11-06 17:44:08 +0700

    #

--- java-core/src/main/java/learn/Main.java
+++ java-core/src/main/java/learn/Main.java
@@ -1,9 +1,31 @@
 package learn;
 
+import java.util.Scanner;
+
 public class Main {
-	
-	public static void main(String[] args) {
-		
+
+	public static boolean isPrime(int n) {
+		if (n < 2) {
+			return false;
+		}
+		int i, sqrt = (int) Math.sqrt(n) + 1;
+		for (i = 2; i < sqrt; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
-	
+
+	public static void main(String[] args) {
+		try (Scanner io = new Scanner(System.in);) {
+			int i, a = io.nextInt(), b = io.nextInt();
+			for (i = a; i <= b; i++) {
+				if (isPrime(i)) {
+					System.out.println(i);
+				}
+			}
+		}
+	}
+
 }
