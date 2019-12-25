fe3abe53f046caa6e6668043a508610840bc814a
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-12-25 09:24:53 +0700

    #

---
+++ java-core/src/main/java/com/basic/UUIDClass.java
@@ -0,0 +1,35 @@
+package com.basic;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+public class UUIDClass {
+
+	public static String bytesToHex(byte[] bytes) throws DecoderException {
+		return Hex.encodeHexString(bytes);
+	}
+
+	public byte[] decodeUsingApacheCommons(String hexString) throws DecoderException {
+		return Hex.decodeHex(hexString.toCharArray());
+	}
+
+	public static void main(String[] args)
+			throws UnsupportedEncodingException, NoSuchAlgorithmException, DecoderException {
+		// UUID uuid = UUID.randomUUID();
+		// int variant = uuid.variant();
+		// System.out.println(variant);
+		// int version = uuid.version();
+		// System.out.println(version);
+
+		MessageDigest salt = MessageDigest.getInstance("SHA-256");
+		salt.update(UUID.randomUUID().toString().getBytes("UTF-8"));
+		String digest = bytesToHex(salt.digest());
+		System.out.println(digest);
+	}
+
+}
