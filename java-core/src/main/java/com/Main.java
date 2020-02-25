539ab870731df05065f6149418ef6f5aea610adb
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2020-02-25 17:45:19 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,33 +1,19 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
-	static void find(int n, int X[], int T[], int i) {
-		int j, k;
-		for (j = X[i - 1]; j <= (n - T[i - 1]); j++) { // X[i-1] <= X[i] <= n - T[i-1]
-			X[i] = j;
-			T[i] = T[i - 1] + j;
-			if (T[i] == n) { // nếu T[i] = n
-				// int k;
-				for (k = 1; k <= i; k++) { // in ra từ X[1] tới X[i]
-					System.out.print(X[k] + " ");
-				}
-				System.out.println();
-			} else {
-				find(n, X, T, i + 1);
+	public static void main(String[] args) throws IOException {
+		int[] a = { 1, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7 };
+		Set<Integer> set = new HashSet<>();
+		for (int k : a) {
+			if (!set.add(k)) {
+				System.out.println("Duplicate: " + k);
 			}
 		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		int n = 5;
-		int[] X = new int[n + 1]; // mảng X có N + 1 phần tử
-		int[] T = new int[n + 1];
-		X[0] = 1;
-		T[0] = 0;
-		find(n, X, T, 1); // mảng nghiệm tính từ i = 1
 	}
 
 }
