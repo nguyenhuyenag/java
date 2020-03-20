f26b0faffbca9e80f816af57658d95cd88dfed9d
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2020-03-20 16:05:55 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void infinity() {
@@ -8,7 +10,17 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		infinity();
+		Random ran = new Random();
+		int cur, pre = ran.nextInt(4);
+		for (int i = 0; i < 10; i++) {
+			cur = ran.nextInt(4);
+			while (cur == pre) {
+				cur = ran.nextInt(4);
+			}
+			pre = cur;
+			System.out.println(cur);
+		}
+
 	}
 
 }
