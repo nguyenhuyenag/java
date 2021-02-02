4b44832e6a81450cd25cd0d6bd1f8751db135df3
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2021-02-02 17:15:07 +0700

    #

--- algorithm/src/main/java/codewars/number/PadovanNumber.java
+++ algorithm/src/main/java/codewars/number/PadovanNumber.java
@@ -1,18 +1,95 @@
 package codewars.number;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+// https://www.codewars.com/kata/5819f1c3c6ab1b2b28000624
 public class PadovanNumber {
 
-	// P(n) = P(n-2) + P(n-3)
-	// 1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12, 16, 21, 28, 37, 49, 65, 86, 114, 151, 200
-	// 0 1 2 3 4
-	public static int get(int n) {
-		// int[] arr = {1, 1, 1, 2};
-		// int i = 0;
-		return 0;
+	// 1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12, 16
+	// P(0) = P(1) = P(2) = 1, P(n) = P(n-2) + P(n-3)
+//	public static int get(int n) {
+//		int pPrevPrev = 1, pPrev = 1, pCurr = 1, pNext = 1;
+//		for (int i = 3; i <= n; i++) {
+//			pNext = pPrevPrev + pPrev;
+//			pPrevPrev = pPrev;
+//			pPrev = pCurr;
+//			pCurr = pNext;
+//		}
+//		return pNext;
+//	}
+
+	static String daoXau(String s) {
+		String s1 = "";
+		for (int i = 0; i < s.length(); i++) {
+			s1 += s.charAt(s.length() - 1 - i);
+		}
+		return s1; // chuoi sau khi dao
+	}
+
+	static String add(String s1, String s2) {
+		String s = "";
+		int len1 = s1.length();
+		int len2 = s2.length();
+		int max = Math.max(len1, len2); // ham max length xuat ra gia tri chieu dai lon nhat trong 2 chuoi
+		s1 = daoXau(s1);
+		s2 = daoXau(s2);
+		// thuc hien noi them so 0 vao chuoi voi muc dich lam cho 2 chuoi so bang nhau
+		if (max > len1) {
+			for (int i = max; i >= len1; i--) {
+				s1 += "0";
+			}
+		}
+		if (max > len2) {
+			for (int i = max; i >= len2; i--) {
+				s2 += "0";
+			}
+		}
+		// thuc hien phep cong
+		int soGhiNho = 0;
+		for (int i = 0; i < max; i++) {
+			int tong = 0;
+			// charAt ky tu tai vi tri thu i trong xau
+			tong = s1.charAt(i) - '0' + s2.charAt(i) - '0' + soGhiNho;
+			s += tong % 10;
+			soGhiNho = tong / 10;
+		}
+		if (soGhiNho == 1) {
+			s += 1;
+		}
+		// thuc hien dao xau truoc khi xuat
+		return daoXau(s);
+	}
+
+	public static BigInteger Get(long n) {
+		String pPrevPrev = "1", pPrev = "1", pCurr = "1", pNext = "1";
+		for (int i = 3; i <= n; i++) {
+			pNext = add(pPrevPrev, pPrev);
+			pPrevPrev = pPrev;
+			pPrev = pCurr;
+			pCurr = pNext;
+		}
+		return new BigInteger(pNext);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(get(6));
+		System.out.println(Get(4));
+	}
+
+	@Test
+	public void ValueTest1() {
+		assertEquals(new BigInteger("1"), PadovanNumber.Get(1));
+		assertEquals(new BigInteger("1"), PadovanNumber.Get(2));
+		assertEquals(new BigInteger("2"), PadovanNumber.Get(3));
+		assertEquals(new BigInteger("2"), PadovanNumber.Get(4));
+	}
+
+	@Test
+	public void ValueTest() {
+		assertEquals(new BigInteger("1177482265857"), PadovanNumber.Get(100));
 	}
 
 }
