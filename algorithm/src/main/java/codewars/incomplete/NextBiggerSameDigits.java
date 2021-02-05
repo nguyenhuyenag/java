e5038cc8d7c98df1ef2c29b86b94cc012fe88269
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2021-02-05 15:11:13 +0700

    #

--- algorithm/src/main/java/codewars/incomplete/NextBiggerSameDigits.java
+++ algorithm/src/main/java/codewars/incomplete/NextBiggerSameDigits.java
@@ -1,41 +1,64 @@
 package codewars.incomplete;
 
-import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+
+import org.junit.Test;
 
 // https://www.codewars.com/kata/55983863da40caa2c900004e
 public class NextBiggerSameDigits {
-	
-	List<Long> list = new ArrayList<>();
-	
-	private static void permute(List<?> list, int left, int right) {
-		if (left == right) {
-			System.out.println(Arrays.toString(list.toArray()));
-			// String.join(",", list);
-			return;
+
+	public static void swap(char[] arr, int i, int j) {
+		char temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	public static long findNext(long n) {
+		char[] arr = String.valueOf(n).toCharArray();
+		int i, len = arr.length;
+		// I) Start from the right most digit and find the
+		// first digit that is smaller than the digit next to it.
+		for (i = len - 1; i > 0; i--) {
+			if (arr[i] > arr[i - 1]) {
+				break;
+			}
 		}
-		for (int j = left; j <= right; j++) {
-			Collections.swap(list, left, j);
-			permute(list, left + 1, right);
-			Collections.swap(list, left, j); // backtrack
+		// If no such digit is found, then all digits are in descending
+		// order means there cannot be a greater number with same set of digits
+		if (i == 0) {
+			return n;
+		} else {
+			int x = arr[i - 1], min = i;
+			// II) Find the smallest digit on right side of (i-1)'th digit that is greater
+			// than number[i-1]
+			for (int j = i + 1; j < len; j++) {
+				if (arr[j] > x && arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			// III) Swap the above found smallest digit with number[i-1]
+			swap(arr, i - 1, min);
+			// IV) Sort the digits after (i-1) in ascending order
+			Arrays.sort(arr, i, len);
+			return Long.parseLong(new String(arr));
 		}
 	}
 
-	public static void showAllPermute(List<?> list) {
-		int size = list.size();
-		permute(list, 0, list.size() - 1);
-	}
-	
 	public static long nextBiggerNumber(long n) {
-		return n;
+		return findNext(n);
 	}
-	
-	public static void main(String[] args) {
-		long n = 123456;
-		int[] arr = Arrays.stream(String.valueOf(n).split("")).mapToInt(Integer::parseInt).toArray();
-		// int max = new StringBui
+
+	@Test
+	public void basicTests() {
+		assertEquals(21, NextBiggerSameDigits.nextBiggerNumber(12));
+		assertEquals(531, NextBiggerSameDigits.nextBiggerNumber(513));
+		assertEquals(2071, NextBiggerSameDigits.nextBiggerNumber(2017));
+		assertEquals(441, NextBiggerSameDigits.nextBiggerNumber(414));
+		assertEquals(414, NextBiggerSameDigits.nextBiggerNumber(144));
+		assertEquals(19009, NextBiggerSameDigits.nextBiggerNumber(10990));
+		// assertEquals(9876543210, NextBiggerSameDigits.nextBiggerNumber(9876543210));
 	}
-	
+
 }
---
+++ algorithm/src/main/java/codewars/incomplete/PowerSumDig.java
@@ -0,0 +1,13 @@
+package codewars.incomplete;
+
+// https://www.codewars.com/kata/55f4e56315a375c1ed000159
+// (1): 81	 => 8 + 1         = 9 => 9^2 = 81
+// (2): 512  => 5 + 1 + 2     = 8 => 8^3 = 512
+// (3): 2401 => 2 + 4 + 0 + 1 = 7 => 7^4 = 2401
+public class PowerSumDig {
+	
+	public static long powerSumDigTerm(int n) {
+        return 0;
+    }
+	
+}
