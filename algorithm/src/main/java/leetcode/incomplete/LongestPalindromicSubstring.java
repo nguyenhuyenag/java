b03d18d9130f8aaede168c386895344f60fbac2b
nguyenhuyenag <nguyenhuyenag@users.noreply.github.com>
2021-02-18 21:02:53 +0700

    #

--- algorithm/src/main/java/leetcode/incomplete/LongestPalindromicSubstring.java
+++ algorithm/src/main/java/leetcode/incomplete/LongestPalindromicSubstring.java
@@ -1,60 +1,53 @@
 package leetcode.incomplete;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 // https://leetcode.com/problems/longest-palindromic-substring/
+// Chuỗi đối xứng dài nhất
 public class LongestPalindromicSubstring {
 
 	public static String getMax(String s1, String s2) {
 		return s1.length() > s2.length() ? s1 : s2;
 	}
 
-	public static boolean isPalindromic(String s) {
-		return s.equals(new StringBuilder(s).reverse().toString());
+	public static boolean isPalindromic(String str) {
+		return str.equals(new StringBuilder(str).reverse().toString());
 	}
 
 	public static String longestPalindrome(String s) {
-		int n = s.length();
-		if (n < 2 || isPalindromic(s)) {
+		if (isPalindromic(s)) {
 			return s;
 		}
 		String max = "";
+		int n = s.length();
 		for (int i = 0; i < n; i++) {
-			String s1 = s.substring(i, n);
-			if (isPalindromic(s1)) {
-				max = getMax(max, s1);
-			}
-			for (int j = s1.length() - 1; j >= 0; j--) {
-				String s2 = s1.substring(0, j);
-				if (isPalindromic(s2)) {
-					max = getMax(max, s2);
+			StringBuilder sb = new StringBuilder();
+			for (int j = i; j < n; j++) {
+				sb.append(s.charAt(j));
+				if (isPalindromic(sb.toString())) {
+					max = getMax(max, sb.toString());
 				}
 			}
 		}
-		if (max.length() > 0) {
-			return max;
-		}
-		return String.valueOf(s.charAt(0));
-	}
-
-	public static void main(String[] args) {
-		String s = "jfbnhnjamsfsbsslcaaivnzryrrkcqmektqjnymeifxvvskicpxxrztysetlpucxfqccmeyptxxziqhacxatxjynmbblssyaxvcmbtyrtqfwxrwsgfrinfkczktytwglbrskeogamecvihkywnljnqfmrrnqcvopcoyroncwzhdqzvwdbtjmcocwljjvipabzorajqgiabqjeyasbrjvyjtdvgupqtmydfgdczaodyokxxarfboxifcltizhhntciffijclljvdfgpsojwmupgtrbquuzjdefnmxtcaborisjcsavucmuovlksonzvmmuvujzirioxdcadeioravjoyxhrqevfwmxacimtvbmfweqpvfijyuqrjfgejrnlmhvbbmbvviilsothgvaqgqtllonrqbsltwpikfrckdhttxzmbqmbhbjjwfddnrfwtafgjtuqyatkpcavokouftqwdzfclkckwzfwlozksgkrcyimvdhfrzlqqxnfhjkwfiewwqmbfyjdjematsvusmqxzwfyukqwlfzzyzlkqvgmq";
-		System.out.println(longestPalindrome(s));
+		return max;
 	}
 
 	@Test
 	public void test() {
-//		assertEquals("aba", longestPalindrome("babad")); // bab
-//		assertEquals("bb", longestPalindrome("cbbd"));
-//		assertEquals("a", longestPalindrome("a"));
-//		assertEquals("a", longestPalindrome("ac"));
-		// assertEquals("cc", longestPalindrome("ccd"));
-		// assertEquals("bb", longestPalindrome("cbbd"));
-		// assertEquals("bb", longestPalindrome("bb"));
-		// assertEquals("bb", longestPalindrome("abb"));
+		assertEquals("bb", longestPalindrome("cbbd"));
+		assertEquals("a", longestPalindrome("a"));
+		assertEquals("cc", longestPalindrome("ccd"));
+		assertEquals("bb", longestPalindrome("cbbd"));
+		assertEquals("bb", longestPalindrome("bb"));
+		assertEquals("bb", longestPalindrome("abb"));
 		assertEquals("aca", longestPalindrome("aacabdkacaa"));
+		assertThat(longestPalindrome("ac"), anyOf(is("a"), is("c")));
+		// assertEquals(longestPalindrome("babad"), anyOf(is("bab"), is("aba")));
 	}
 
 }
