a9b7853ee2a7a2a9e736f4e43622d0a3fc83eccc
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-04-01 17:17:36 +0700

    #

--- java-core/src/main/java/com/basic/thread/RunnableDemo.java
+++ java-core/src/main/java/com/basic/thread/RunnableDemo.java
@@ -5,7 +5,7 @@ public class RunnableDemo implements Runnable {
 	private Thread t;
 	private String threadName;
 
-	RunnableDemo(String name) {
+	public RunnableDemo(String name) {
 		threadName = name;
 		System.out.println("Creating " + threadName);
 	}
