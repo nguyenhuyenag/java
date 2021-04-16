da84aae520656a5e454339f5eb5576d1cc4fbe66
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-04-16 08:40:56 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,18 +1,17 @@
 package com;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// Path path = Paths.get("D:\\Dev\\Project\\Github\\java\\java-core\\src\\main\\java\\common\\util\\Colors.java");
-		//List<String> list = FilesUtils.readAllLines(path);
-		//System.out.println(list);
+		// Path path =
+		// Paths.get("D:\\Dev\\Project\\Github\\java\\java-core\\src\\main\\java\\common\\util\\Colors.java");
+		// List<String> list = FilesUtils.readAllLines(path);
+		// System.out.println(list);
 		// Path path = Paths.get("C:\\Users\\nguye\\Desktop\\test\\data.txt");
-		//Path path = Paths.get("D:\\GDrive\\ToCompany\\maple\\api\\src");
-		//byte[] arr = "Java 7".getBytes();
+		// Path path = Paths.get("D:\\GDrive\\ToCompany\\maple\\api\\src");
+		// byte[] arr = "Java 7".getBytes();
 //		boolean append = true;
 //		for (int i = 1; i < 16; i++) {
 //			String s = "Java " + i;
@@ -23,11 +22,11 @@ public class Main {
 //		list.add("Văn");
 //		list.add("Huyện");
 //		FilesUtils.writeTextToFile(path, list, false);
-		//List<String> list = FilesUtils.listFile(path);
-		//System.out.println(list.size());
-		//System.out.println(StringUtils.isEmpty(""));
-		//System.out.println("OK");
-		
+		// List<String> list = FilesUtils.listFile(path);
+		// System.out.println(list.size());
+		// System.out.println(StringUtils.isEmpty(""));
+		// System.out.println("OK");
+
 //		Path path = Paths.get("C:\\Users\\huyennv\\Desktop\\text.txt");
 //		System.out.println(StringUtils.capitalize("abc"));
 //		FilesUtils.readAllLines(path).forEach(t -> {
@@ -35,9 +34,7 @@ public class Main {
 //			System.out.println("\tcongty.set"+StringUtils.capitalize(t)+"(LDAPAttribute.getAttributeValue(entry, FieldThongTinCongTy."+t.toUpperCase()+"));");
 //			System.out.println("}");
 //		});
-		
-		List<String> list = new ArrayList<>();
-		System.out.println(list.isEmpty());
+
 	}
 
 }
