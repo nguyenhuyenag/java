5b0f59b9152439b31d0ab7c7b6ac392a945df34a
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-05-10 17:18:38 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,10 +1,25 @@
 
 package com;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.text.CaseUtils;
+
 public class Main {
 
-	public static void main(String[] args) {
+	private static String toCamel(String str) {
+		return CaseUtils.toCamelCase(str, false, new char[] { '_' });
+	}
 
+	public static void main(String[] args) {
+		List<String> keys = Arrays.asList("opened_by", "ticket_owner", "close_reason", "full_name");
+		List<String> fields = Arrays.asList("openedBy", "ticketOwner", "closeReason");
+		keys.forEach(t -> {
+			if (fields.contains(toCamel(t))) {
+				System.out.println(t);
+			}
+		});
 	}
 
 }
