baa8d49ad75e1112551513d13df707313134c35f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-05-23 21:35:40 +0700

    update

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,14 +1,9 @@
 package com;
 
-import java.util.StringTokenizer;
-
 public class Main {
 
 	public static void main(String[] args) {
-		  StringTokenizer st = new StringTokenizer("this is a test");
-		     while (st.hasMoreTokens()) {
-		         System.out.println(st.nextToken());
-		     }
+
 	}
 
 }
--- java-core/src/main/java/common/util/Base64Utils.java
+++ java-core/src/main/java/common/util/Base64Utils.java
@@ -1,11 +1,8 @@
 package common.util;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-
-import javax.activation.DataHandler;
 
 public class Base64Utils {
 	
@@ -18,6 +15,61 @@ public class Base64Utils {
 		return Base64.getEncoder().encode(byteArr);
 	}
 	
+	public static String encodeToString(byte[] byteArr) {
+		byte[] arr = encode(byteArr);
+		return new String(arr, StandardCharsets.UTF_8);
+	}
+	
+	public static String encodeToString(String str) {
+		byte[] byteArr = str.getBytes(StandardCharsets.UTF_8);
+		return encodeToString(byteArr);
+	}
+	
+	public static String encodeToString(File file) {
+		byte[] byteArr = FilesUtils.toByteArray(file);
+		return encodeToString(byteArr);
+	}
+	
+//	/**
+//	 * Base64 encode with UTF-8
+//	 * @param src the string to encode
+//	 * @return byte array encoded
+//	 */
+//	public static byte[] encode(String src) {
+//		return encode(src, StandardCharsets.UTF_8);
+//	}
+
+//	/**
+//	 * Base64 encode to string with UTF-8
+//	 * @param byteArr the byte array to encode
+//	 * @return string encoded
+//	 */
+//	public static String encodeToString(byte[] byteArr) {
+//		return encodeToString(byteArr, StandardCharsets.UTF_8);
+//	}
+	
+//	/**
+//	 * Base64 encode DataHandler
+//	 * @param path of file
+//	 * @return
+//	 * @throws IOException
+//	 */
+//	public static String encode(DataHandler handler) {
+//		byte[] byteArr = FilesUtils.toByteArray(handler);
+//		return encodeToString(byteArr);
+//	}
+	
+//	/**
+//	 * Base64 encode DataHandler without Charset
+//	 * @param path of file
+//	 * @return
+//	 * @throws IOException
+//	 */
+//	public static String encodeWithoutCharset(DataHandler handler) {
+//		byte[] byteArr = FilesUtils.toByteArray(handler);
+//		return Base64.getEncoder().encodeToString(byteArr);
+//	}
+	
 	/**
 	 * Base64 decode
 	 * @param src the byte array to decode
@@ -28,105 +80,42 @@ public class Base64Utils {
 	}
 
 	/**
-	 * Base64 decode with Charset
-	 * @param src the string to encode
-	 * @param charset is Charset
-	 * @return byte array encoded
-	 */
-	public static byte[] encode(String src, Charset charset) {
-		byte[] byteArr = src.getBytes(charset);
-		return encode(byteArr);
-	}
-
-	/**
-	 * Base64 encode with UTF-8
-	 * @param src the string to encode
-	 * @return byte array encoded
-	 */
-	public static byte[] encode(String src) {
-		return encode(src, StandardCharsets.UTF_8);
-	}
-
-	/**
-	 * Base64 encode to string with Charset
-	 * @param byteArr the byte array to encode
-	 * @param charset is Charset
-	 * @return string encoded
-	 */
-	public static String encodeToString(byte[] byteArr, Charset charset) {
-		byte[] arr = encode(byteArr);
-		return new String(arr, charset);
-	}
-
-	/**
-	 * Base64 encode to string with UTF-8
-	 * @param byteArr the byte array to encode
-	 * @return string encoded
-	 */
-	public static String encodeToString(byte[] byteArr) {
-		return encodeToString(byteArr, StandardCharsets.UTF_8);
-	}
-	
-	/**
-	 * Base64 encode DataHandler
-	 * @param path of file
-	 * @return
-	 * @throws IOException
-	 */
-	public static String encode(DataHandler handler) {
-		byte[] byteArr = FilesUtils.toByteArray(handler);
-		return encodeToString(byteArr);
-	}
-	
-	/**
-	 * Base64 encode DataHandler without Charset
-	 * @param path of file
-	 * @return
-	 * @throws IOException
-	 */
-	public static String encodeWithoutCharset(DataHandler handler) {
-		byte[] byteArr = FilesUtils.toByteArray(handler);
-		return Base64.getEncoder().encodeToString(byteArr);
-	}
-
-	/**
-	 * Base64 decode with Charset
-	 * @param src the string Base64
-	 * @param charset is Charset
+	 * Base64 decode with UTF-8
+	 * @param base64 is a String Base64
 	 * @return byte array decoded
 	 */
-	public static byte[] decode(String src, Charset charset) {
-		byte[] byteArr = src.getBytes(charset);
+	public static byte[] decode(String base64) {
+		byte[] byteArr = base64.getBytes(StandardCharsets.UTF_8);
 		return decode(byteArr);
 	}
 
-	/**
-	 * Base64 decode with UTF-8
-	 * @param src the string Base64
-	 * @return byte array decoded
-	 */
-	public static byte[] decode(String src) {
-		return decode(src, StandardCharsets.UTF_8);
-	}
+//	/**
+//	 * Base64 decode with UTF-8
+//	 * @param src the string Base64
+//	 * @return byte array decoded
+//	 */
+//	public static byte[] decode(String src) {
+//		return decode(src, StandardCharsets.UTF_8);
+//	}
 
-	/**
-	 * Base64 decode to string with Charset
-	 * @param src the string Base64
-	 * @param charset is Charset
-	 * @return string decoded
-	 */
-	public static String decodeToString(String src, Charset charset) {
-		byte[] byteArr = decode(src, charset);
-		return new String(byteArr, charset);
-	}
+//	/**
+//	 * Base64 decode to string with Charset
+//	 * @param src the string Base64
+//	 * @param charset is Charset
+//	 * @return string decoded
+//	 */
+//	public static String decodeToString(String src, Charset charset) {
+//		byte[] byteArr = decode(src, charset);
+//		return new String(byteArr, charset);
+//	}
 
-	/**
-	 * Base64 decode to string with UTF-8
-	 * @param src the string Base64
-	 * @return string decoded
-	 */
-	public static String decodeToString(String base64) {
-		return decodeToString(base64, StandardCharsets.UTF_8);
-	}
+//	/**
+//	 * Base64 decode to string with UTF-8
+//	 * @param src the string Base64
+//	 * @return string decoded
+//	 */
+//	public static String decodeToString(String base64) {
+//		return decodeToString(base64, StandardCharsets.UTF_8);
+//	}
 
 }
