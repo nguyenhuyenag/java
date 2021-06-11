277a63b45ca4bd5070be8260bff9c3ffe6cca061
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-06-11 16:47:22 +0700

    update

---
+++ java-core/src/main/java/com/collection/iterable/IterableSize.java
@@ -0,0 +1,15 @@
+package com.collection.iterable;
+
+import java.util.stream.StreamSupport;
+
+public class IterableSize {
+
+	public long size1(Iterable<String> data) {
+		return data.spliterator().getExactSizeIfKnown();
+	}
+
+	public long size(Iterable<String> data) {
+		return StreamSupport.stream(data.spliterator(), false).count();
+	}
+
+}
