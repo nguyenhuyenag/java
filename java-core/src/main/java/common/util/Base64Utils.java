9a32490fefb7bf06912b470edaab0fd807d74fac
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-01-11 10:03:57 +0700

    #

--- java-core/src/main/java/common/util/Base64Utils.java
+++ java-core/src/main/java/common/util/Base64Utils.java
@@ -1,6 +1,9 @@
 package common.util;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Base64;
 
 public class Base64Utils {
@@ -18,6 +21,17 @@ public class Base64Utils {
 		byte[] byteArr = str.getBytes(StandardCharsets.UTF_8);
 		return encodeToString(byteArr);
 	}
+	
+	public static String encodeToString(Path path) {
+		try {
+			byte[] byteArr = Files.readAllBytes(path);
+			byteArr = Base64.getEncoder().encode(byteArr);
+			return new String(byteArr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
 
 	public static byte[] decodeToByte(String base64) {
 		byte[] byteArr = base64.getBytes(StandardCharsets.UTF_8);
@@ -30,5 +44,5 @@ public class Base64Utils {
 		byteArr = Base64.getDecoder().decode(byteArr);
 		return new String(byteArr);
 	}
-
+	
 }
