8ca32b66663da409bdca4eb6c0f2cd42e015f410
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-02-25 17:14:51 +0700

    #

--- java-core/src/main/java/com/basic/thread/executors/ExeService.java
+++ java-core/src/main/java/com/basic/thread/executors/ExeService.java
@@ -17,10 +17,28 @@ import java.util.concurrent.Future;
  * - submit(Callable): Tương tự execute() nhưng có trả về kết quả thông qua
  * Future.
  * 
- * - submit(Runnable): Cho phép Thread này trả kết quả về một cách đồng bộ. Tức
- * là khi Thread kết thúc thì null mới được trả về thông qua future.get(), nên
- * dù được gọi rất sớm, nhưng chúng chỉ in ra null khi nào Thread kết thúc.
+ * - submit(Runnable): Tương tự submit(Callable) nhưng trả kết quả về một cách
+ * đồng bộ. Tức là khi Thread kết thúc thì null mới được trả về thông qua
+ * future.get(), nên dù được gọi rất sớm, nhưng chúng chỉ in ra null khi nào
+ * Thread kết thúc.
  * 
+ * - invokeAny(Runnable): Tương tự execute() hay submit() nhưng cho phép ta thể
+ * truyền vào chúng danh sách các Callable. Hàm thực thi các Callable theo quy
+ * luật khai báo Thead Pool, nhưng khi có bất kỳ Callable nào hoàn thành trong
+ * danh sách các Callable truyền vào đó, Thread Pool sẽ chấm dứt các Thread còn
+ * lại, dù cho chúng đã được đưa vào Pool và đang chờ thực thi.
+ * 
+ * - invokeAll(Runnable): Thực thi tất cả các Callable và chờ nhận các kết quả
+ * trả về của các Callable này thông qua danh sách các đối tượng Future.
+ * 
+ * - shutdown() và shutdownNow(): Một ExecutorService không tự động kết thúc khi
+ * chúng thực thi hết các Thread, nó vẫn ở đó và khiến ứng dụng vẫn chạy mặc dù
+ * các Thread trong Thread Pool đã hoàn thành. Lệnh shutdown() giúp đóng Thread
+ * Pool lại, Thread Pool lúc này sẽ từ chối nhận thêm task nữa và giúp
+ * ExecutorService kết thúc sau khi nó hoàn thành nhiệm vụ. Tương tự,
+ * shutdownNow() cũng có công năng như vậy, chỉ khác một chỗ phương thức này
+ * buộc ExecutorService kết thúc ngay khi được gọi, lúc này đây các Thread chưa
+ * được thực thi sẽ bị buộc phải kết thúc theo ExecutorService.
  */
 public class ExeService {
 
--- java-core/src/main/java/com/basic/thread/sync/SynchronizedMethods.java
+++ java-core/src/main/java/com/basic/thread/sync/SynchronizedMethods.java
@@ -5,12 +5,26 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
-public class Main {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SynchronizedMethods {
+
+	private int sum = 0;
+
+	public void calculate() {
+		setSum(getSum() + 1);
+	}
+
+	public synchronized void syncCalculate() {
+		setSum(getSum() + 1);
+	}
 
 	public static void main(String[] args) throws InterruptedException {
 		ExecutorService service = Executors.newFixedThreadPool(3);
-		BaeldungSynchronizedMethods summation = new BaeldungSynchronizedMethods();
-
+		SynchronizedMethods summation = new SynchronizedMethods();
 		IntStream.range(0, 1000).forEach(count -> service.submit(summation::calculate));
 		service.awaitTermination(1000, TimeUnit.MILLISECONDS);
 		System.out.println(summation.getSum());
