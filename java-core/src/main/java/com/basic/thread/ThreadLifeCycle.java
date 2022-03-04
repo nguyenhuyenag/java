9aa6872cfbe379dc961c2dbe7550e07e3d385dc4
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-03-04 14:26:36 +0700

    #

--- java-core/src/main/java/com/basic/thread/ThreadLifeCycle.java
+++ java-core/src/main/java/com/basic/thread/ThreadLifeCycle.java
@@ -5,7 +5,10 @@ package com.basic.thread;
  * 
  * - NEW: Thread được khởi tạo nhưng chưa start
  * 
- * - RUNNABLE: Thread được start
+ * - RUNNABLE: Thread được start. Sau start thì thread vẫn chưa chạy liền mà còn
+ * chờ cấp phát tài nguyên, do đó chúng ta có thể chia thành 2 trạng thái nhỏ
+ * nữa là Ready to Run (chờ đợi cấp phát tài nguyên) và Running (đã chính thức
+ * chạy).
  * 
  * - Trong quá trình chạy, nếu có bất kỳ tác động nào, ngoại trừ làm kết thúc
  * vòng đời của thread (TERMINATED), nó sẽ vào trạng thái dưới đây
@@ -25,14 +28,19 @@ package com.basic.thread;
  * 
  * - TERMINATED A thread that has exited is in this state.
  * 
- * @author huyennv
- *
  */
-public class ThreadLifeCycle {
+public class ThreadLifeCycle extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println("Thread start");
+	}
 
 	public static void main(String[] args) {
-		FromClassThread t = new FromClassThread();
-		t.getState(); //
+		ThreadLifeCycle t = new ThreadLifeCycle();
+		System.out.println(t.getState()); // NEW
+		t.start();
+		System.out.println(t.getState()); // RUNNABLE
 	}
 
 }
