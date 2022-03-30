0dafc2712c34d625048d8c1608995af41b1f9590
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-03-30 08:14:34 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,9 +1,23 @@
 package com;
 
+import com.chilkatsoft.CkZip;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
+	static {
+		try {
+			System.loadLibrary("chilkat");
+		} catch (UnsatisfiedLinkError e) {
+			System.err.println("Native code library failed to load.\n" + e);
+			System.exit(1);
+		}
+	}
+
+	// Instantiate a Chilkat object and print it's version.
+	public static void main(String argv[]) {
+		CkZip zip = new CkZip();
+		System.out.println(zip.version());
+		System.out.println("TS24................");
 	}
 
 }
