cdeae23f1479ecf37beff6efbfc24555177be314
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-06-15 14:27:36 +0700

    #

--- java-core/src/main/java/com/basic/charstring/LoremIpsums.java
+++ java-core/src/main/java/com/basic/charstring/LoremIpsums.java
@@ -1,17 +1,17 @@
-//package com.basic.charstring;
-//
-//import com.thedeanda.lorem.Lorem;
-//import com.thedeanda.lorem.LoremIpsum;
-//
-//public class LoremIpsums {
-//
-//	public static void main(String[] args) {
-//		Lorem lorem = LoremIpsum.getInstance();
-//		String text = lorem.getWords(10);
-//		// String text = lorem.getParagraphs(1, 15);
-//		// Path path = Paths.get("file/lorem.txt");
-//		//FilesUtils.appendStringToFile(path, text);
-//		System.out.println(text);
-//	}
-//
-//}
+package com.basic.charstring;
+
+import com.thedeanda.lorem.Lorem;
+import com.thedeanda.lorem.LoremIpsum;
+
+public class LoremIpsums {
+
+	public static void main(String[] args) {
+		Lorem lorem = LoremIpsum.getInstance();
+		// String text = lorem.getWords(10);
+		// String text = lorem.getParagraphs(1, 15);
+		// Path path = Paths.get("file/lorem.txt");
+		//FilesUtils.appendStringToFile(path, text);
+		System.out.println(lorem.getStateFull());
+	}
+
+}
