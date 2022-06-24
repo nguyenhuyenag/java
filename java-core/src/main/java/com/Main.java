7aa498507d0e5681695bdf03174babf4f8bc7ae3
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-06-24 08:32:39 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -2,8 +2,17 @@ package com;
 
 public class Main {
 
+	public static final String[] WHITE_LIST = { "/v1/**", "/auth/**" };
+
 	public static void main(String[] args) {
-		System.out.println(false & false);
+		// System.out.println(false & false);
+		String url = "/v1/slow-service";
+		for (int i = 0; i < WHITE_LIST.length; i++) {
+			WHITE_LIST[i] = WHITE_LIST[i].replaceAll("\\*", "");
+			if (url.startsWith(WHITE_LIST[i])) {
+				System.out.println("In white list");
+			}
+		}
 	}
 
 }
