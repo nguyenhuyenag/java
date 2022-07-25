ef156fdefacb86793fa49d45ce1205b4704960f3
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-07-25 09:27:34 +0700

    #

---
+++ java-core/src/main/java/common/util/zip/IoUtils.java
@@ -0,0 +1,23 @@
+package common.util.zip;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class IoUtils {
+
+	private IoUtils() {
+	}
+
+	public static void closeAll(Closeable... cls) {
+		for (Closeable c : cls) {
+			if (c != null)
+				try {
+					c.close();
+				} catch (IOException ex) {
+					ex.printStackTrace();
+					// Logger.getLogger(IoUtils.class.getName()).log(Level.SEVERE, null, ex);
+				}
+		}
+	}
+
+}
