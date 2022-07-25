9fb19f87d912915138d85860b177ebd1c7930009
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-07-25 17:14:29 +0700

    #

---
+++ java-core/src/main/java/com/basic/thread/countdownlatch/CountDownLatchDemo.java
@@ -0,0 +1,94 @@
+package com.basic.thread.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+class Sevice1 extends Thread {
+
+	private CountDownLatch countDownLatch;
+
+	public Sevice1(CountDownLatch countDownLatch) {
+		this.countDownLatch = countDownLatch;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Thread 1 done, Id = " + Thread.currentThread().getId());
+		this.countDownLatch.countDown();
+	}
+
+}
+
+class Sevice2 extends Thread {
+
+	private CountDownLatch countDownLatch;
+
+	public Sevice2(CountDownLatch countDownLatch) {
+		this.countDownLatch = countDownLatch;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Thread 2 done, Id = " + Thread.currentThread().getId());
+		this.countDownLatch.countDown();
+	}
+
+}
+
+class Sevice3 extends Thread {
+
+	private CountDownLatch countDownLatch;
+
+	public Sevice3(CountDownLatch countDownLatch) {
+		this.countDownLatch = countDownLatch;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Thread 3 done, Id = " + Thread.currentThread().getId());
+		this.countDownLatch.countDown();
+	}
+
+}
+
+public class CountDownLatchDemo {
+
+	public static void main(String[] args) {
+		final CountDownLatch latch = new CountDownLatch(3);
+		
+		Thread service1 = new Thread(new Sevice1(latch));
+		Thread service2 = new Thread(new Sevice2(latch));
+		Thread service3 = new Thread(new Sevice3(latch));
+		
+		service1.start();
+		service2.start();
+		service3.start();
+
+		// latch waits till the count becomes 0
+		// this way we can make sure that the execution of main thread only
+		// finishes ones 3 services have executed
+		try {
+			System.out.println("Waiting for 3 services have started ... ");
+			latch.await(); // 
+			System.out.println("Starting main Thread ... ");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Done!!!");
+	}
+
+}
