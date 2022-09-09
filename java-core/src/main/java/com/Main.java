40cb64c12144eab91775a88c2b6cc9aa5b60dc02
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-09-09 11:38:07 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,7 +1,5 @@
 package com;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.StringSelection;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -10,32 +8,6 @@ import com.basic.ClipboardUtils;
 import common.util.Base64Utils;
 
 public class Main {
-
-//	public static boolean containsDuplicate(int[] nums) {
-//		Set<Integer> set = new HashSet<>();
-//		for (int x : nums) {
-//			if (!set.add(x)) {
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-
-//	public boolean isAnagram(String s, String t) {
-//		int n = s.length();
-//		if (n != t.length())
-//			return false;
-//		char[] arrs = s.toCharArray();
-//		char[] arrt = t.toCharArray();
-//		Arrays.sort(arrs);
-//		Arrays.sort(arrt);
-//		for (int i = 0; i < n; i++) {
-//			if (arrs[i] != arrt[i]) {
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
 
 	public static void main(String[] args) {
 		Path path = Paths.get("C:/Users/huyennv/Desktop/tax-conver/05_GTGT_TT80_2022.xlsx");
