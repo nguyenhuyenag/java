adb799be42ea66802d597d34b9ef4a1743204086
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-09-20 16:48:15 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -4,53 +4,32 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import com.basic.ClipboardUtils;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import common.util.Base64Utils;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-class A {
-
-	String name;
-	int age;
-
-}
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-class B extends A {
-
-	@JsonIgnore
-	String name;
-	int height;
-
-}
 
 public class Main {
 
-	public static void main(String[] args) throws JsonProcessingException {
-		// test();
-		B bean = new B("Lac", 177);
-		bean.setAge(24);
-		System.out.println(new ObjectMapper().writeValueAsString(bean));
+	public static void test() {
+		// public static void main (String[] args) throws java.lang.Exception
+		{
+			String errorMessage = null;
+			try {
+				throw (new Exception("Let's throw some exception message here"));
+			} catch (Exception e) {
+				System.out.println("In Exception block.");
+				errorMessage = e.getMessage();
+			} finally {
+				System.out.println(errorMessage);
+			}
+		}
 	}
 
-	public static void test() {
-		Path path = Paths.get("C:/Users/huyennv/Desktop/tax-conver/05_GTGT_TT80_2022.xlsx");
-		String base64 = Base64Utils.encodeToString(path);
-		System.out.println(base64.trim());
-		ClipboardUtils.copy(base64);
+	public static void main(String[] args) {
+//		Path path = Paths.get("C:/Users/huyennv/Desktop/tax-conver/test.xlsx");
+//		String base64 = Base64Utils.encodeToString(path);
+//		System.out.println(base64.trim());
+//		ClipboardUtils.copy(base64);
+		test();
 	}
 
 }
