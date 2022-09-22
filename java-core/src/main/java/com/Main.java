4a0f156a0156bbb61ab43eb123dce5718a544183
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-09-22 17:07:54 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -9,27 +9,11 @@ import common.util.Base64Utils;
 
 public class Main {
 
-	public static void test() {
-		// public static void main (String[] args) throws java.lang.Exception
-		{
-			String errorMessage = null;
-			try {
-				throw (new Exception("Let's throw some exception message here"));
-			} catch (Exception e) {
-				System.out.println("In Exception block.");
-				errorMessage = e.getMessage();
-			} finally {
-				System.out.println(errorMessage);
-			}
-		}
-	}
-
 	public static void main(String[] args) {
-//		Path path = Paths.get("C:/Users/huyennv/Desktop/tax-conver/test.xlsx");
-//		String base64 = Base64Utils.encodeToString(path);
-//		System.out.println(base64.trim());
-//		ClipboardUtils.copy(base64);
-		test();
+		Path path = Paths.get("C:/Users/huyennv/Desktop/tax-conver/824_01_LPMB_TT80_2022.xlsx");
+		String base64 = Base64Utils.encodeToString(path);
+		System.out.println(base64.trim());
+		ClipboardUtils.copy(base64);
 	}
 
 }
