79e99eac541c6b8bddefcc0d6694c7fb06c8a4db
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-10-02 18:36:09 +0700

    test

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -53,11 +53,9 @@ public class Main {
 
 		Map<String, Long> result = items.stream()
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
 		// Sort a map and add to finalMap
 		result.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed())
 				.forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
-
 		System.out.println(finalMap);
 	}
 
