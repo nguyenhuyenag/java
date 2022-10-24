c0bff905af3143981fe68d61b6b3864c03c8701b
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-10-24 15:39:10 +0700

    #

--- java-9/src/main/java/com/Main.java
+++ java-9/src/main/java/com/Main.java
@@ -4,8 +4,29 @@ import java.util.stream.Stream;
 
 public class Main {
 
+	public static void infoOfCurrentProcess() {
+		ProcessHandle processHandle = ProcessHandle.current();
+		ProcessHandle.Info processInfo = processHandle.info();
+		System.out.println("PID: " + processHandle.pid());
+		System.out.println("Arguments: " + processInfo.arguments());
+		System.out.println("Command: " + processInfo.command());
+		System.out.println("Instant: " + processInfo.startInstant());
+		System.out.println("Total CPU duration: " + processInfo.totalCpuDuration());
+		System.out.println("User: " + processInfo.user());
+	}
+
+	public static void infoOfLiveProcesses() {
+		Stream<ProcessHandle> liveProcesses = ProcessHandle.allProcesses();
+		liveProcesses.filter(ProcessHandle::isAlive).forEach(ph -> {
+			System.out.println("PID: " + ph.pid());
+			System.out.println("Instance: " + ph.info().startInstant());
+			System.out.println("User: " + ph.info().user());
+		});
+	}
+
 	public static void main(String[] args) {
-		Stream.iterate(0, i -> i < 10, i -> i + 1).forEach(System.out::println);
+		// infoOfCurrentProcess();
+		infoOfLiveProcesses();
 	}
 
 }
