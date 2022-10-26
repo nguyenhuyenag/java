8bc2e06dd5b8880382369767233c2fdd78efde67
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-10-26 14:14:06 +0700

    #

---
+++ java-10/src/main/java/com/LocalVariableTypeInference.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.List;
+
+public class LocalVariableTypeInference {
+
+	public static void main(String[] args) {
+		String s1 = "Welcome to"; 	// old
+		var s2 = "Java 10"; 		// new
+		System.out.printf("%s %s", s1, s2);
+
+		var names = List.of("Julie", "Robert", "Chris", "Joseph");
+		for (var name : names) {
+			System.out.println(name);
+		}
+	}
+
+}
---
+++ java-10/src/main/java/com/UnmodifiableCollections.java
@@ -0,0 +1,28 @@
+package com;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UnmodifiableCollections {
+
+	// copyOf(), toUnmodifiableList(), toUnmodifiableSet(), toUnmodifiableMap()
+	public static void main(String[] args) {
+		var ids = List.of(1, 2, 3, 4, 5);
+		try {
+			List<Integer> copyOfIds = List.copyOf(ids); // get an unmodifiable list
+			copyOfIds.add(6);
+		} catch (UnsupportedOperationException e) {
+			System.out.println("Collection is not modifiable");
+		}
+		try {
+			// get an unmodifiable list
+			List<Integer> evenNumbers = ids.stream() //
+					.filter(i -> i % 2 == 0) //
+					.collect(Collectors.toUnmodifiableList());
+			evenNumbers.add(6);
+		} catch (UnsupportedOperationException e) {
+			System.out.println("Collection is not modifiable");
+		}
+	}
+
+}
