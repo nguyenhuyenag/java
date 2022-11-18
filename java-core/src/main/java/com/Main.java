97d5e23d580e254f81fb739436bee9aa3f0f2bab
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-11-18 16:38:26 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,21 +1,29 @@
 package com;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-//	public static void test() {
-//		Path path = Paths.get("C:/Users/huyennv/Desktop/tax-conver/892_03_TNDN_TT80_251.xlsx");
-//		String base64 = Base64Utils.encodeToString(path);
-//		System.out.println(base64.trim());
-//		ClipboardUtils.copy(base64);
-//	}
+	private final AtomicInteger counter = new AtomicInteger(0);
 
-	private final static String HOME = System.getProperty("user.dir");
-	
+	public int getValue() {
+		return counter.get();
+	}
+
+	public void increment() {
+		System.out.println("increment");
+		while (true) {
+			int existingValue = getValue();
+			int newValue = existingValue + 1;
+			if (counter.compareAndSet(existingValue, newValue)) {
+				return;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		String url = "jdbc:mysql://192.168.0.51:3306/invoice_master?useUnicode=true&characterEncoding=utf-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Asia/Ho_Chi_Minh";
-		System.out.println(StringUtils.substringBetween(url, "://", ":"));
+		Main m = new Main();
+		m.increment();
 	}
 
 }
