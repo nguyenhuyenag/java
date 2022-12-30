30e402026f6d17da9f3d9ede572ce94a17f1f5bd
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-12-30 08:08:28 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,18 +1,9 @@
 package com;
 
-import com.basic.json.general.JSONProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import common.util.JSONUtils;
-
 public class Main {
 
-	public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
-		String json = "{\"_id\": 12, \"name\": \"Green\"}";
-		JSONProperty bean = new ObjectMapper().readerFor(JSONProperty.class).readValue(json);
-		System.out.println(JSONUtils.toJSON(bean));
+	public static void main(String[] args) {
+		
 	}
 
 }
