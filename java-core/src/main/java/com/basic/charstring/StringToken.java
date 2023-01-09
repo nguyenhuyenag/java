07ba1296c6dd8e59ded82ecbb3d8843bbac3969e
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-01-09 16:39:23 +0700

    #

--- java-core/src/main/java/com/basic/charstring/StringToken.java
+++ java-core/src/main/java/com/basic/charstring/StringToken.java
@@ -5,8 +5,8 @@ import java.util.StringTokenizer;
 public class StringToken {
 
 	public static void main(String[] args) {
-		String property = System.getProperty("java.library.path");
-		StringTokenizer parser = new StringTokenizer(property, ";");
+		String property = "C:/WINDOWS/System32/OpenSSH/";
+		StringTokenizer parser = new StringTokenizer(property, "/"); // delim = " " is default
 		while (parser.hasMoreTokens()) {
 			System.err.println(parser.nextToken());
 		}
