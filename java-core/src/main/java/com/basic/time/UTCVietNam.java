81af58b2226adbe5edc6382135261e133645effb
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-03-28 16:39:23 +0700

    #

--- java-core/src/main/java/com/basic/time/UTCVietNam.java
+++ java-core/src/main/java/com/basic/time/UTCVietNam.java
@@ -9,9 +9,10 @@ public class UTCVietNam {
 	
 	public static void main(String[] args) {
 		ZonedDateTime utc = ZonedDateTime.now(ZoneOffset.UTC);
-		System.out.println("UTC = " + utc.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+		DateTimeFormatter ft = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		System.out.println("UTC = " + utc.format(ft));
 		ZonedDateTime vn = ZonedDateTime.now(ZoneId.of("Asia/Bangkok"));
-		System.out.println("Viet Nam = " + vn.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+		System.out.println("Viet Nam = " + vn.format(ft));
 	}
 	
 }
