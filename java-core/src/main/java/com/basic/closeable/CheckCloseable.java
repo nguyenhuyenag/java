c7897db9c2dbea99085c564ee194237cafa245fb
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-04-19 16:52:26 +0700

    #

---
+++ java-core/src/main/java/com/basic/closeable/CheckCloseable.java
@@ -0,0 +1,17 @@
+package com.basic.closeable;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class CheckCloseable {
+
+	public static <T> boolean isAutoCloseable(T obj) {
+		return (obj instanceof AutoCloseable);
+	}
+
+	public static void main(String[] args) {
+		CloseableHttpClient client = HttpClients.createDefault();
+		System.out.println(isAutoCloseable(client));
+	}
+
+}
