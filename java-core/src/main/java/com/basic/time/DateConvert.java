b1e5b0adc76f58e4d97de19144824a5e68e2f357
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-05-20 23:05:56 +0700

    #

---
+++ java-core/src/main/java/com/basic/time/DateConvert.java
@@ -0,0 +1,5 @@
+package com.basic.time;
+
+public class DateConvert {
+
+}
--- java-core/src/main/java/com/basic/time/DaysBetweenTwoDate.java
+++ java-core/src/main/java/com/basic/time/DaysBetweenTwoDate.java
@@ -7,27 +7,41 @@ import java.util.concurrent.TimeUnit;
 
 public class DaysBetweenTwoDate {
 
-	public static long getDifferenceDays(Date d1, Date d2) {
-		long diff = d2.getTime() - d1.getTime();
-		return TimeUnit.DAYS.convert(diff, TimeUnit.DAYS);
+	// public static Date asDate(LocalDate localDate) {
+	// return
+	// Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+	// }
+	//
+	// public static Date asDate(LocalDateTime localDateTime) {
+	// return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+	// }
+
+	public static long betweenDates(Date d1, Date d2, TimeUnit unit) throws IOException {
+		return ChronoUnit.DAYS.between(d1.toInstant(), d2.toInstant());
 	}
 
-	public static long betweenDates(Date firstDate, Date secondDate) throws IOException {
-		return ChronoUnit.DAYS.between(firstDate.toInstant(), secondDate.toInstant());
+//	public static long call1(LocalDate d1, LocalDate d2, TimeUnit unit) {
+//		long epoch1 = d1.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
+//		long epoch2 = d2.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
+//		return convertTimeUnit(Math.abs(epoch1 - epoch2), unit);
+//	}
+
+	private static long convertTimeUnit(Date d1, Date d2, TimeUnit unit) {
+		long diff = Math.abs(d1.getTime() - d2.getTime());
+		// return unit.convert(diff, unit);
+		System.out.println("Diff of d1 & d2: " + Math.abs(diff));
+		// millisecond -> other
+		return unit.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	//	public static Date asDate(LocalDate localDate) {
-	//		return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
-	//	}
-	//
-	//	public static Date asDate(LocalDateTime localDateTime) {
-	//		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-	//	}
 
 	public static void main(String[] args) {
-		// LocalDate d1 = LocalDate.of(1987, Month.JUNE, 24);
-		// LocalDate d2 = LocalDate.of(2007, Month.JULY, 15);
-		// int days = Days.daysBetween(past,now).getDays();
+		// LocalDate d1 = LocalDate.of(2023, Month.JUNE, 10);
+		// LocalDate d2 = LocalDate.of(2023, Month.JUNE, 15);
+		// long result = call1(d1, d2, TimeUnit.DAYS);
+		Date d1 = new Date(2023, 10, 12);
+		Date d2 = new Date(2023, 10, 20);
+		long diff = convertTimeUnit(d1, d2, TimeUnit.HOURS);
+		System.out.println(diff);
 	}
 
 }
