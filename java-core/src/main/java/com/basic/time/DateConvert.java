48fddec07af4d6bc5e17cc6fa490628f6cff7f12
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-05-21 10:08:09 +0700

    #

--- java-core/src/main/java/com/basic/time/DateConvert.java
+++ java-core/src/main/java/com/basic/time/DateConvert.java
@@ -1,5 +1,79 @@
 package com.basic.time;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 public class DateConvert {
+
+	// Date -> LocalDate
+	public static LocalDate dateToLocalDate(Date date) {
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	// Date -> LocalDateTime
+	public static LocalDateTime dateToLocalDateTime(Date date) {
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
+
+	// Date -> Instant
+	public static Instant dateToInstant(Date date) {
+		return Instant.ofEpochMilli(date.getTime());
+	}
+
+	// LocalDate -> Date
+	public static Date localDateToDate(LocalDate localDate) {
+		return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	}
+
+	// LocalDate -> LocalDateTime
+	public static LocalDateTime localDateToLocalDateTime(LocalDate localDate) {
+		return localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
+
+	// LocalDate -> Instant
+	public static Instant localDateToLocalInstant(LocalDate localDate) {
+		LocalDateTime localDateTime = localDate.atStartOfDay();
+		return localDateTime.toInstant(ZoneOffset.UTC);
+	}
+
+	// Instant -> Date
+	public static Date instantToDate(Instant instant) {
+		return Date.from(instant);
+	}
+
+	// Instant -> LocalDate
+	public static LocalDate instantToLocalDate(Instant instant) {
+		ZonedDateTime zonedDateTime = instant.atZone(ZoneId.systemDefault());
+		return zonedDateTime.toLocalDate();
+	}
+
+	// Instant -> LocalDateTime
+	public static LocalDateTime instantToLocalDateTime(Instant instant) {
+		return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+	}
+
+	public static long getEpochTime(Object dateTimeObject) {
+		if (dateTimeObject instanceof Date) {
+			Date date = (Date) dateTimeObject;
+			Instant instant = date.toInstant();
+			return instant.toEpochMilli();
+		} else if (dateTimeObject instanceof LocalDate) {
+			LocalDate localDate = (LocalDate) dateTimeObject;
+			LocalDateTime localDateTime = localDate.atStartOfDay();
+			Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
+			return instant.toEpochMilli();
+		} else if (dateTimeObject instanceof LocalDateTime) {
+			LocalDateTime localDateTime = (LocalDateTime) dateTimeObject;
+			Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
+			return instant.toEpochMilli();
+		} else {
+			throw new IllegalArgumentException("Unsupported DateTime object type");
+		}
+	}
 
 }
--- java-core/src/main/java/com/basic/time/DaysBetweenTwoDate.java
+++ java-core/src/main/java/com/basic/time/DaysBetweenTwoDate.java
@@ -7,29 +7,12 @@ import java.util.concurrent.TimeUnit;
 
 public class DaysBetweenTwoDate {
 
-	// public static Date asDate(LocalDate localDate) {
-	// return
-	// Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
-	// }
-	//
-	// public static Date asDate(LocalDateTime localDateTime) {
-	// return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-	// }
-
 	public static long betweenDates(Date d1, Date d2, TimeUnit unit) throws IOException {
 		return ChronoUnit.DAYS.between(d1.toInstant(), d2.toInstant());
 	}
 
-//	public static long call1(LocalDate d1, LocalDate d2, TimeUnit unit) {
-//		long epoch1 = d1.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
-//		long epoch2 = d2.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
-//		return convertTimeUnit(Math.abs(epoch1 - epoch2), unit);
-//	}
-
-	private static long convertTimeUnit(Date d1, Date d2, TimeUnit unit) {
+	private static long convertTimeUnit(TimeUnit unit, Date d1, Date d2) {
 		long diff = Math.abs(d1.getTime() - d2.getTime());
-		// return unit.convert(diff, unit);
-		System.out.println("Diff of d1 & d2: " + Math.abs(diff));
 		// millisecond -> other
 		return unit.convert(diff, TimeUnit.MILLISECONDS);
 	}
@@ -40,7 +23,7 @@ public class DaysBetweenTwoDate {
 		// long result = call1(d1, d2, TimeUnit.DAYS);
 		Date d1 = new Date(2023, 10, 12);
 		Date d2 = new Date(2023, 10, 20);
-		long diff = convertTimeUnit(d1, d2, TimeUnit.HOURS);
+		long diff = convertTimeUnit(TimeUnit.HOURS, d1, d2);
 		System.out.println(diff);
 	}
 
