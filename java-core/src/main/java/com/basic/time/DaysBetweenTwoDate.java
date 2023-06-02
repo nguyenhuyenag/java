90d290800d0669ad243ce93ba6018afa5fe9f3f9
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-02 08:57:33 +0700

    #

--- java-core/src/main/java/com/basic/time/DaysBetweenTwoDate.java
+++ java-core/src/main/java/com/basic/time/DaysBetweenTwoDate.java
@@ -3,7 +3,12 @@ package com.basic.time;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DaysBetweenTwoDate {
+
+	private static Logger LOG = LoggerFactory.getLogger(DaysBetweenTwoDate.class);
 
 //	public static long betweenDates(Date d1, Date d2, TimeUnit unit) throws IOException {
 //		return ChronoUnit.DAYS.between(d1.toInstant(), d2.toInstant());
@@ -14,7 +19,13 @@ public class DaysBetweenTwoDate {
 		return DateConvert.convertTimeUnit(diff, TimeUnit.MILLISECONDS, timeUnit);
 	}
 
-	public static long calculatorDiff(TimeUnit timeUnit, Object d1, Object d2) {
+	public static long calculatorDiff(Object d1, Object d2, TimeUnit timeUnit) {
+		// Check d1, d2 are of the same class
+		boolean result = d1.getClass().equals(d2.getClass());
+		if (!result) {
+			LOG.error("Two objects aren't of the same class");
+			return -1;
+		}
 		long dt1 = DateConvert.getEpochTime(d1);
 		long dt2 = DateConvert.getEpochTime(d2);
 		return calculatorDiff(timeUnit, dt1, dt2);
@@ -27,7 +38,8 @@ public class DaysBetweenTwoDate {
 		// LocalDate d2 = LocalDate.of(2023, Month.OCTOBER, 20);
 		LocalDateTime d1 = LocalDateTime.of(2023, 1, 1, 15, 30, 10);
 		LocalDateTime d2 = LocalDateTime.of(2023, 12, 1, 15, 30, 10);
-		long diff = calculatorDiff(TimeUnit.HOURS, d1, d2);
+
+		long diff = calculatorDiff(d1, d2, TimeUnit.HOURS);
 		System.out.println(diff);
 	}
 
