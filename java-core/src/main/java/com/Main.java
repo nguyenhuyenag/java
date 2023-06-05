5811093e3dc7e2f37e372cd2b649fbbf9b3bc057
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-05 09:03:45 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,9 +1,15 @@
 package com;
 
+import java.util.Calendar;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance(); // set this up however you need it.
+		cal.add(Calendar.DATE, 5);
+		int day = cal.get(Calendar.DAY_OF_WEEK);
 		
+		System.out.println(day);
 	}
 
 }
