885f68d82424f7f3b4ee5e0683b376100039aa07
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-26 17:03:32 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,9 +1,19 @@
 package com;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tika.Tika;
+
+import common.util.PathUtils;
+
 public class Main {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		String path = PathUtils.PROJECT_DIR + "/file/data.json";
+		File file = new File(path);
+		Tika tika = new Tika();
+		System.out.println(tika.detect(file));
 	}
 
 }
---
+++ java-core/src/main/java/com/basic/file/DetectMimeType.java
@@ -0,0 +1,26 @@
+package com.basic.file;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tika.Tika;
+
+import common.util.PathUtils;
+
+public class DetectMimeType {
+
+	public static void main(String[] args) throws IOException {
+		String path = PathUtils.PROJECT_DIR + "/file/data.json";
+		File file = new File(path);
+		Tika tika = new Tika();
+		System.out.println(tika.detect(file));
+		System.out.println(tika.detect(file));
+//		try (InputStream is = new BufferedInputStream(new FileInputStream(file));) {
+//			String mimeType = URLConnection.guessContentTypeFromStream(is);
+//			System.out.println(mimeType);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+	}
+
+}
