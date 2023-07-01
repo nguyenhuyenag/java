5ed2b5458a436359fd83747179cfdd599be598b4
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-07-01 21:03:29 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -3,7 +3,11 @@ package com;
 public class Main {
 
 	public static void main(String[] args) {
-
+		 if (args.length > 0) {
+		        System.out.println(args[0]);
+		    } else {
+		        System.out.println("No command line arguments were provided.");
+		    }
 	}
 
 }
