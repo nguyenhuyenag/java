e22d1273016a7744dce1d2c642c9e220ef180aaa
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-07-02 13:04:18 +0700

    #

--- java-core/src/main/java/com/basic/charstring/CharacterApi.java
+++ java-core/src/main/java/com/basic/charstring/CharacterApi.java
@@ -4,14 +4,23 @@ public class CharacterApi {
 
 	public static void main(String[] args) {
 		// Check lower case
-		System.out.println(Character.isLowerCase('a'));
+		System.out.println("IsLowerCase: " + Character.isLowerCase('a'));
+
 		// Check upper case
-		System.out.println(Character.isUpperCase('A'));
+		System.out.println("IsUpperCase: " + Character.isUpperCase('A'));
+
 		// Check digit
-		System.out.println(Character.isDigit('1'));
+		System.out.println("IsDigit: " + Character.isDigit('1'));
 		char ch3 = 'あ'; // Chữ cái tiếng Nhật Hiragana
-		System.out.println(Character.isLetter(ch3)); 	 // Determines if the specified character is a letter 
-		System.out.println(Character.isAlphabetic(ch3)); // Determines if the specified character (Unicode code point) is alphabetic 
+		System.out.println("IsLetter: " + Character.isLetter(ch3)); // Determines if the specified character is a letter
+		System.out.println("IsAlphabetic: " + Character.isAlphabetic(ch3)); // Determines if the specified character
+																			// (Unicode code point)
+
+		/*-
+		 * digit: Số nguyên cần chuyển đổi thành ký tự. 
+		 * radix: Hệ cơ số, có giá trị từ 2 đến 36.
+		 */
+		System.out.println("ForDigit: " + Character.forDigit(10, 16));
 	}
-	
+
 }
