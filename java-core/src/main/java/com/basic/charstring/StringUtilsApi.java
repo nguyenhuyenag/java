3e93b712872524a5ccf8d08874ed469dee77d30f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-07-04 22:14:58 +0700

    #

--- java-core/src/main/java/com/basic/charstring/StringUtilsApi.java
+++ java-core/src/main/java/com/basic/charstring/StringUtilsApi.java
@@ -1,7 +1,9 @@
 package com.basic.charstring;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class StringUtilsApi {
-	
+
 	/**
 	 * - isAnyEmpty(): Có ít nhất 1 phần tử là empty
 	 * 
@@ -9,14 +11,31 @@ public class StringUtilsApi {
 	 * 
 	 * - isAllEmpty():
 	 * 
+	 * - stripAccents(): Xóa dấu (vẫn còn xót "đ")
+	 * 
+	 * - equals():
+	 * - equalsIgnoreCase():
+	 * - equalsAny(): Kiểm tra giá trị có bằng với giá trị bất kỳ trong danh sách
+	 * - equalsAnyIgnoreCase():
+	 * 
+	 * - indexOf():
+	 * - lastIndexOf():
+	 * - indexOfAny():
+	 * 
+	 * - contains():
+	 * - containsIgnoreCase():
+	 * - containsAny():
 	 * 
 	 */
 	private static void method() {
-		
+		// equalsAny
+		String value = "apple";
+		boolean isEqualsAny = StringUtils.equalsAny(value, "apple", "banana", "orange");
+		System.out.println("EqualsAny: " + isEqualsAny); // Output: true
 	}
-	
+
 	public static void main(String[] args) {
 		method();
 	}
-	
+
 }
