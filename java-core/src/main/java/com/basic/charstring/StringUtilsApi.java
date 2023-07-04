9880577b01cafa83e7f00f365060d177602f3161
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-07-04 17:16:43 +0700

    #

--- java-core/src/main/java/com/basic/charstring/StringUtilsApi.java
+++ java-core/src/main/java/com/basic/charstring/StringUtilsApi.java
@@ -6,9 +6,13 @@ public class StringUtilsApi {
 	 * - isAnyEmpty(): Có ít nhất 1 phần tử là empty
 	 * 
 	 * - isNoneEmpty(): Có ít nhất 1 phần tử là NOT empty
+	 * 
+	 * - isAllEmpty():
+	 * 
+	 * 
 	 */
 	private static void method() {
-		isAnyEmpty(null);
+		
 	}
 	
 	public static void main(String[] args) {
