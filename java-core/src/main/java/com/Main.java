52e7cb1e1731ab76a1a3054b27e2ed1814757ab1
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-07-18 20:57:23 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,35 +1,8 @@
 package com;
 
-enum MyEnum {
-
-	PULL_UP {
-		@Override
-		public int getValue() {
-			return 1;
-		}
-	},
-	PULL_DOWN {
-		@Override
-		public int getValue() {
-			return 0;
-		}
-	};
-
-	public abstract int getValue();
-
-}
-
 public class Main {
 
 	public static void main(String[] args) {
-		int myInt = 1;
-		MyEnum myEnum = MyEnum.PULL_DOWN;
-		System.out.println(MyEnum.PULL_DOWN);
-		// Using equals() method
-		if (myEnum.equals(myInt)) {
-			System.out.println("Enum and int are equal");
-		} else {
-			System.out.println("Enum and int are not equal");
-		}
+
 	}
 }
--- java-core/src/main/java/com/trick/InitValueInIfCondition.java
+++ java-core/src/main/java/com/trick/InitValueInIfCondition.java
@@ -2,14 +2,15 @@ package com.trick;
 
 public class InitValueInIfCondition {
 	
-	public static void init(final CharSequence cs) {
+	public static boolean init(final CharSequence cs) {
         int strLen;
         if (cs == null || (strLen = cs.length()) == 0) { // <- here
             return true;
         }
         for (int i = 0; i < strLen; i++) {
-            
+            // 
         }
+        return true;
     }
 	
 }
