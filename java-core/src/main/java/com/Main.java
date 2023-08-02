09104386ea980ae9c128c3afc3dbe061025a3def
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-08-02 16:07:47 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -8,13 +8,14 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Optional<String> valueOptional = Optional.of("Hello");
 
-        // Sử dụng map() để biến đổi giá trị trong Optional từ kiểu String sang kiểu Integer
-        Optional<String> lengthOptional = valueOptional.map(str -> str + "123");
+		// Sử dụng map() để biến đổi giá trị trong Optional từ kiểu String sang kiểu
+		// Integer
+		Optional<String> lengthOptional = valueOptional.map(str -> str + "123");
 
-        if (lengthOptional.isPresent()) {
-            System.out.println("Length of the value: " + lengthOptional.get());
-        } else {
-            System.out.println("No value found.");
-        }
+		if (lengthOptional.isPresent()) {
+			System.out.println("Length of the value: " + lengthOptional.get());
+		} else {
+			System.out.println("No value found.");
+		}
 	}
 }
