f09927ab1a75ee3196cd64b41035398cf7eb656f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-08-14 14:11:55 +0700

    #

--- java-core/src/main/java/com/basic/enums/EnumsApi.java
+++ java-core/src/main/java/com/basic/enums/EnumsApi.java
@@ -5,16 +5,15 @@ package com.basic.enums;
  *
  * - compareTo(): So sánh hằng enum dựa trên giá trị thứ tự
  * 
- * 		Size.SMALL.compareTo(Size.MEDIUM) // returns (ordinal(SMALL) - ordinal(MEDIUM))
+ * 		Size.SMALL.compareTo(Size.MEDIUM)	// returns (ordinal(SMALL) - ordinal(MEDIUM))
  *
- * - name(): Trả về tên xác định một hằng số enum ở dạng chuỗi. Giá trị trả về
- * từ phương thức name() là final
+ * - name(): Trả về tên xác định một hằng số enum ở dạng chuỗi. Giá trị trả về từ phương thức name() là final
  * 
  * 		name(SMALL) // returns "SMALL"
  *
  * - valueOf(): Nhận một chuỗi và trả về một hằng số enum có tên chuỗi tương tự
  *
- * 		Size.valueOf("SMALL") // returns constant SMALL
+ * 		Size.valueOf("SMALL") 				// returns constant SMALL
  * 
  * - values(): Trả về một mảng của kiểu enum chứa tất cả các hằng số enum
  * 
