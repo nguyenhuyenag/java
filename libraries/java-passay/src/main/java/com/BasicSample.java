4322a10ebd9059217d4f6a5fd3b5e6c580646aa8
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-09-03 14:29:19 +0700

    #

---
+++ libraries/java-passay/src/main/java/com/BasicSample.java
@@ -0,0 +1,30 @@
+package com;
+
+import org.passay.LengthRule;
+import org.passay.PasswordData;
+import org.passay.PasswordValidator;
+import org.passay.RuleResult;
+import org.passay.RuleResultDetail;
+import org.passay.RuleResultMetadata;
+
+public class BasicSample {
+
+	public static void main(String[] args) {
+		PasswordValidator passwordValidator = new PasswordValidator(new LengthRule(5));
+		PasswordData passwordData = new PasswordData();
+		passwordData.setPassword("1234");
+		RuleResult validate = passwordValidator.validate(passwordData);
+		System.out.println("IsValid: " + validate.isValid());
+
+		RuleResultDetail ruleResultDetail = validate.getDetails().get(0);
+		System.out.println(ruleResultDetail.getErrorCode());
+		System.out.println(ruleResultDetail.getParameters().get("minimumLength"));
+		System.out.println(ruleResultDetail.getParameters().get("maximumLength"));
+
+		Integer lengthCount = validate //
+				.getMetadata() //
+				.getCounts() //
+				.get(RuleResultMetadata.CountCategory.Length);
+	}
+
+}
