8465b74a198cc680bd2e2980b32b45009e2952f4
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-09-29 08:58:35 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,19 +1,10 @@
 package com;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		String s = "Java 2023";
-		InputStream is = new ByteArrayInputStream(s.getBytes());
-//		try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-//			for (String line; (line = reader.readLine()) != null;) {
-//				System.out.println(line);
-//			}
-//		}
-		is.mark(0);
+		System.out.println(true | false);
+		System.out.println(true & true);
 	}
 
 }
