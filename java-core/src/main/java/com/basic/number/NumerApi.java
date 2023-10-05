79b452b1c5b72be4a3b9d7b0ed8001506d6206d8
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-10-05 09:52:46 +0700

    #

---
+++ java-core/src/main/java/com/basic/number/NumerApi.java
@@ -0,0 +1,12 @@
+package com.basic.number;
+
+public class NumerApi {
+
+	public static void main(String[] args) {
+		int a = 7, b = 3;
+		// a = 2 * b + 1
+		System.out.println(a / b); // Chia nguyên (quotient)
+		System.out.println(a % b); // Chia dư (remainder)
+	}
+
+}
