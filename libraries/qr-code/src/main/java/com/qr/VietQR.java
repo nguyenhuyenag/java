b9e03a9c756d9284bff5b284f331a2a38e182d46
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-11-14 21:23:24 +0700

    #

--- libraries/qr-code/src/main/java/com/qr/VietQR.java
+++ libraries/qr-code/src/main/java/com/qr/VietQR.java
@@ -29,21 +29,23 @@ public class VietQR {
 		httpPost.setHeader("x-api-key", "e97a6eb8-eed5-471d-8e63-ab4f396b7862");
 
 		Map<String, String> params = Map.of( //
-			"acqId", "970424", //
-			"amount", "123456", // Tong so tien thanh toan
-			"addInfo", "Thong tin chuyen khoan", // Thong tin chuyen khoan
-			"format", "text", //
-			"template", "compact2", //
-			"accountNo", "113366668888", //
-			"accountName", "QUY VAC XIN PHONG CHONG COVID" //
+				"acqId", "970424", //
+				"amount", "123456", // Tong so tien thanh toan
+				"addInfo", "Thong tin chuyen khoan", // Thong tin chuyen khoan
+				"format", "text", //
+				"template", "compact2", //
+				"accountNo", "113366668888", //
+				"accountName", "QUY VAC XIN PHONG CHONG COVID" //
 		);
 
 		String jsonPayload = new ObjectMapper().writeValueAsString(params);
 		httpPost.setEntity(new StringEntity(jsonPayload, ContentType.APPLICATION_JSON));
 
-		try (CloseableHttpClient client = HttpClients.createDefault()) {
-			ClassicHttpResponse response = client.execute(httpPost, new BasicHttpContext());
-
+		try ( //
+			CloseableHttpClient client = HttpClients.createDefault();
+			ClassicHttpResponse response = client.execute(httpPost, new BasicHttpContext()); //
+			// ClassicHttpResponse response = client.execute(httpPost, new HttpClientContext()); //
+		) {
 			// Get the response body
 			HttpEntity responseEntity = response.getEntity();
 			String responseBody = EntityUtils.toString(responseEntity);
