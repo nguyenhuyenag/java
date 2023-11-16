05c84443c21411856c8b6aeaa28bf93ded81ca5e
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-11-16 11:30:08 +0700

    #

--- libraries/java-json/src/main/java/com/json/preview/JACKSONInject.java
+++ libraries/java-json/src/main/java/com/json/preview/JACKSONInject.java
@@ -1,8 +1,6 @@
 package com.json.preview;
 import com.fasterxml.jackson.annotation.JacksonInject;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.InjectableValues;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.json.JsonUtils;
 
@@ -13,7 +11,7 @@ public class JACKSONInject {
 
 	public String name;
 
-	public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
+	public static void main(String[] args) throws Exception {
 		String json = "{\"name\": \"Injectable Values\"}";
 
 		InjectableValues inject = new InjectableValues.Std() //
