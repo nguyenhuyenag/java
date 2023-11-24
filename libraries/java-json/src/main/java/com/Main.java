2718c41c6cd1c4334479f2c6cc94badfccb78c06
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-11-24 09:38:56 +0700

    #

--- libraries/java-json/src/main/java/com/Main.java
+++ libraries/java-json/src/main/java/com/Main.java
@@ -1,21 +1,34 @@
 package com;
 
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.nio.file.Paths;
 
-import org.apache.commons.lang3.time.StopWatch;
+import org.apache.commons.lang3.SystemUtils;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor  
+@AllArgsConstructor  
+@Getter  
+class Car { 
+	
+    @JsonSetter("carBrand")  
+    private String brand;  
+    
+}
 
 public class Main {
 
-	public static void main(String[] args) {
-		final StopWatch watch = new StopWatch();
-		watch.start();
-		try {
-			TimeUnit.SECONDS.sleep(2);
-		} catch (final InterruptedException ex) {
-		}
-		System.out.println(watch.getTime(TimeUnit.SECONDS));
-		System.out.println(watch.getTime());
-		watch.stop();
+	public static void main(String[] args) throws Exception {
+		File file = Paths.get(SystemUtils.USER_DIR, "file/car.json").toFile();  
+	    ObjectMapper objectMapper = new ObjectMapper();
+		Car car = objectMapper.readValue(file, Car.class); 
+		System.out.println(car.getBrand());
 	}
 
 }
---
+++ libraries/java-json/src/main/java/com/json/parse/FieldSetter.java
@@ -0,0 +1,46 @@
+package com.json.parse;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/*-
+ * + @JsonSetter cho phép Jackson xác định tên của thuộc tính Java tương ứng với một key trong JSON
+ * 
+ * 			@JsonSetter("carBrand") 
+ * 			private String brand;
+ * 
+ * 	 hoặc
+ * 
+ * 			@JsonSetter("name") 
+ * 			public void setName(String name) { 
+ * 				this.name = name;
+ * 			}
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+class Car {
+
+	@JsonSetter("carBrand")
+	private String brand;
+
+}
+
+public class FieldSetter {
+
+	protected static void jsonSetter() throws Exception {
+		String json = "{ \"carBrand\" : \"BMW\"  }";
+		ObjectMapper objectMapper = new ObjectMapper();
+		Car car = objectMapper.readValue(json, Car.class);
+		System.out.println(car.getBrand());
+	}
+
+	public static void main(String[] args) throws Exception {
+		jsonSetter();
+	}
+
+}
