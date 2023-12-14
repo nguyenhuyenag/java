31333fb86105c03f7da224f984515b8c3d0ca39f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-14 14:37:37 +0700

    #

--- intellij/intellij-project/src/main/java/com/v14/HelpfulNullPointerException.java
+++ intellij/intellij-project/src/main/java/com/v14/HelpfulNullPointerException.java
@@ -2,13 +2,13 @@ package com.v14;
 
 /*-
  * String name = jd.getBlog().getAuthor()
- * 
- * // Before v14 
- * Exception in thread "main" java.lang.NullPointerException 
+ *
+ * - Before v14
+ * Exception in thread "main" java.lang.NullPointerException
  * 		at NullPointerExample.main(NullPointerExample.java:5)
- * 
- * // For v14
- * Exception in thread "main" java.lang.NullPointerException: Cannot invoke "Blog.getAuthor()" 
+ *
+ * - For v14
+ * Exception in thread "main" java.lang.NullPointerException: Cannot invoke "Blog.getAuthor()"
  * because the return value of "Journaldev.getBlog()" is null
  * 		at NullPointerExample.main(NullPointerExample.java:4)
  */
--- intellij/intellij-project/src/main/java/com/v14/SwitchExpresstion.java
+++ intellij/intellij-project/src/main/java/com/v14/SwitchExpresstion.java
@@ -3,32 +3,32 @@ package com.v14;
 import org.apache.commons.lang3.StringUtils;
 
 /*-
-  - Yield isn’t a new keyword in Java. It’s just used in switch expressions
-  
-    // Ternary operator
-	var isMonday = (day == Day.Monday ? true : false);
+- Yield isn’t a new keyword in Java. It’s just used in switch expressions
 
-	// Switch expression
-	var isMonday = switch (day) {
-	    case MONDAY -> true;
-	    default -> false;
-	};
- */
+// Ternary operator
+var isMonday = (day == Day.Monday ? true : false);
+
+// Switch expression
+var isMonday = switch (day) {
+case MONDAY -> true;
+default -> false;
+};
+*/
 public class SwitchExpresstion {
-	
-	public static void main(String[] args) {
-		String dayOfWeek = "2";
-		String result = switch (dayOfWeek) {
-			case "2", "3", "4", "5", "6" -> "Working Day";
-			case "0", "7" -> "Day Off";
-			default -> {
-				if (StringUtils.isEmpty(dayOfWeek))
-					yield "Please insert a valid day";
-				else
-					yield "Looks like wrong!";
-			}
-		};
-		System.out.println(result);
-	}
+
+    public static void main(String[] args) {
+        String dayOfWeek = "2";
+        String result = switch (dayOfWeek) {
+            case "2", "3", "4", "5", "6" -> "Working Day";
+            case "0", "7" -> "Day Off";
+            default -> {
+                if (StringUtils.isEmpty(dayOfWeek))
+                    yield "Please insert a valid day";
+                else
+                    yield "Looks like wrong!";
+            }
+        };
+        System.out.println(result);
+    }
 
 }
