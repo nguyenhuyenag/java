f9cce62daa961f30feadd0ce3d1f9eb00f6d6d0f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-28 22:30:13 +0700

    #

--- java-core/src/main/java/com/Main.java
+++ java-core/src/main/java/com/Main.java
@@ -1,25 +1,9 @@
 package com;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        char[] arr = new char[]{'a', 'b', 'c', 'd', 'f'};
-        int[] charCount = new int[256];
 
-        for (char c : arr) {
-            charCount[c]++;
-        }
-
-        for (int i = 0; i < charCount.length; i++) {
-            // Ví dụ: charCount[i] = charCount['a']
-            if (Character.isLetter(i)) {
-                System.out.println(i + ", " + (char) i + " : " + charCount[i]);
-            }
-        }
     }
 
 }
