f5c54043e92e850f3f444b8aa6a6c5fb55d312e3
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-07 18:47:43 +0700

    #

--- java-core/src/main/java/com/MainCore.java
+++ java-core/src/main/java/com/MainCore.java
@@ -5,17 +5,56 @@ import java.util.Stack;
 
 public class MainCore {
 
-    public static void main(String[] args) throws Exception {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(5);
-        stack.push(0);
-        stack.push(0);
-        stack.push(9);
-        stack.push(4);
-        stack.push(8);
-        while (!stack.isEmpty()) {
-            System.out.print(stack.pop() + " ");
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Hoán đổi các phần tử qua đường chéo chính (transpose the matrix)
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
         }
+
+        printMatrix(matrix);
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------");
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        printMatrix(matrix);
+
+        rotate(matrix);
+
+        printMatrix(matrix);
+
     }
 
 }
