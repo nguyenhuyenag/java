b09f5afb85729fe21318acf7a9c9807aaa4cc3d0
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-17 22:26:51 +0700

    #

--- java-core/src/main/java/com/MainCore.java
+++ java-core/src/main/java/com/MainCore.java
@@ -3,8 +3,7 @@ package com;
 public class MainCore {
 
     public static void main(String[] args) {
-        String s = "tree";
-        System.out.println("s = " + Arrays.toString(s.getBytes()));
+
     }
 
 }
