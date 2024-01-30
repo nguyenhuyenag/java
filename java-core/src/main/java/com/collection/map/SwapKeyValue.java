b6a3a2621d55f47ad493ab08ea8df54fb7887c5b
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-30 10:48:55 +0700

    #

---
+++ java-core/src/main/java/com/collection/map/SwapKeyValue.java
@@ -0,0 +1,23 @@
+package com.collection.map;
+
+import org.apache.commons.collections4.MapUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SwapKeyValue {
+
+    private <A, B> Map<B, A> invertMap(Map<A, B> map) {
+        Map<B, A> reverseMap = new HashMap<>();
+        for (Map.Entry<A, B> entry : map.entrySet()) {
+            reverseMap.put(entry.getValue(), entry.getKey());
+        }
+        return reverseMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Character> myHashMap = new HashMap<>();
+        Map<Character, String> inversedMap = new HashMap<>();
+        myHashMap.forEach((key, value) -> inversedMap.put(value, key));
+    }
+}
