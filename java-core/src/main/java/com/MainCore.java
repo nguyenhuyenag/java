61a532372e0e9475ad0cfb021b88a8b7139c7c47
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-14 15:47:09 +0700

    #

--- java-core/src/main/java/com/MainCore.java
+++ java-core/src/main/java/com/MainCore.java
@@ -2,22 +2,8 @@ package com;
 
 public class MainCore {
 
-//    public static String findOrigial(String s) {
-//        StringBuilder result = new StringBuilder();
-//        int[] mark = new int[26];
-//        for (char c : s.toCharArray()) {
-//            mark[c - 'a']++;
-//        }
-//        for (int i = 0; i < 26; i++) {
-//            if (mark[i] > 0) {
-//                result.append(String.valueOf((char) (i + 'a')).repeat(mark[i]));
-//            }
-//        }
-//        return result.toString();
-//    }
-
     public static void main(String[] args) {
-        // findOrigial("caaaaba");
+
     }
 
 }
