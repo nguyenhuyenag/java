cb3ea6d1af589fcc8fa27dee07b1096f52f8c694
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-03-21 17:15:12 +0700

    #

--- java-core/src/main/java/com/MainCore.java
+++ java-core/src/main/java/com/MainCore.java
@@ -3,11 +3,17 @@ package com;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class MainCore {
 
     public static void main(String[] args) {
-        int x  = 10;
-        System.out.println("Integer.reverse(x); = " + Integer.reverse(x));
+        BigInteger n1 = new BigInteger("987");
+        BigInteger n2 = new BigInteger("-987");
+        BigInteger n3 = new BigInteger("123");
+        System.out.println(n1.mod(n3));
+        System.out.println(n1.remainder(n3));
     }
 
 }
