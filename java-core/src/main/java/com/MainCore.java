a755a095d1cc332782f8eb371483ae9a02010456
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-04-01 23:25:08 +0700

    #

--- java-core/src/main/java/com/MainCore.java
+++ java-core/src/main/java/com/MainCore.java
@@ -5,16 +5,13 @@ import common.object.User;
 import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Random;
 
 public class MainCore {
 
     public static void main(String[] args) {
-        User u1 = new User("An", 22);
-        User u2 = u1;
-        u2.setAge(1_000);
-        System.out.println(u1);
-        System.out.println(u2);
+
     }
 
 }
