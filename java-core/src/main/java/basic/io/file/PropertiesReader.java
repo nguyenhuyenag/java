6e2dd2f2eadbc7b7fb8b5223247b192466ae709a
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-05-18 19:51:11 +0700

    #

--- java-core/src/main/java/basic/io/file/PropertiesReader.java
+++ java-core/src/main/java/basic/io/file/PropertiesReader.java
@@ -6,16 +6,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class PropertiesReader {
 	
-	private static final Logger LOG = LoggerFactory.getLogger(PropertiesReader.class);
+	// private static final Logger LOG = LoggerFactory.getLogger(PropertiesReader.class);
 	
 	private final static String HOME = System.getProperty("user.dir");
 	
@@ -33,14 +36,15 @@ public class PropertiesReader {
 	}
 	
 	public static void readFile() {
-		System.out.println("HOME: " + HOME);
-		try (InputStream input = new FileInputStream(HOME + "/config.properties");) {
+		// System.out.println("HOME: " + HOME);
+		Path path = Path.of(HOME, "src", "main", "resources", "config.properties");
+		try (InputStream input = new FileInputStream(path.toString())) {
 			Properties prop = new Properties();
             // load a properties file
             prop.load(input);
-            System.out.println(prop.getProperty("server.port"));
-		} catch (IOException io) {
-			io.printStackTrace();
+			prop.forEach((key, value) -> System.out.println(key + " <==> " + value));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
@@ -52,7 +56,7 @@ public class PropertiesReader {
 		// Resource resource = new ClassPathResource(fileName); // (1)
 		File resource = new File(fileName); // (2)
 		if (!resource.exists()) {
-			LOG.info("File '{}' not found!", fileName);
+			log.info("File '{}' not found!", fileName);
 		} else {
 			// try (InputStream inputStream = resource.getInputStream()) { 		//	(1)
 			try (InputStream inputStream = new FileInputStream(resource)) { 	// 	(2)
@@ -67,7 +71,7 @@ public class PropertiesReader {
 	}
 
 	public static void main(String[] args) throws IOException {
-		
+		readFile();
 	}
 
 }
