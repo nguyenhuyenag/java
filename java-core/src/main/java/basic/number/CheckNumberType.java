57025ac0c2f89711b652d5c6fd7ea370271e4d71
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-05-26 11:32:29 +0700

    #

--- java-core/src/main/java/basic/number/CheckNumberType.java
+++ java-core/src/main/java/basic/number/CheckNumberType.java
@@ -7,7 +7,8 @@ public class CheckNumberType {
     public static void checkIntergerType() {
         final List<Object> list = List.of(1, 2, "aasf", "1", "123", 123);
         for (Object obj : list) {
-            System.out.println("%s -> %s".formatted(obj, Integer.class.isInstance(obj)));
+            // System.out.println("%s -> %s".formatted(obj, Integer.class.isInstance(obj)));
+            System.out.println("%s -> %s".formatted(obj, obj instanceof Integer));
         }
     }
 
