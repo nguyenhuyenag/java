7f309b9736d32dc38bd19cb2e80a7fecd147f85d
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-06-10 17:15:40 +0700

    #

--- java-core/src/main/java/test/MainCore.java
+++ java-core/src/main/java/test/MainCore.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 
 import java.io.File;
@@ -8,23 +9,20 @@ import java.io.File;
 public class MainCore {
 
     public static void main(String[] args) {
-        System.out.println(SystemUtils.USER_HOME);
-        System.out.println(SystemUtils.USER_DIR);
-        // System.out.println(System.getProperty("java.class.path"));
+        // System.out.println(SystemUtils.USER_HOME);
+        // System.out.println(SystemUtils.USER_DIR);
 
         // Lấy đường dẫn thư mục chứa file được thực thi
-        String pathToExecutable = MainCore.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        String projectDirectory = FileUtils.getFile(pathToExecutable).getParent();
+        String classes = MainCore.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        System.out.println("classes = " + classes);
 
-        // Kiểm tra xem thư mục gốc của dự án có chứa tệp pom.xml không
-        File pomFile = new File(projectDirectory, "pom.xml");
-        if (!pomFile.exists()) {
-            // Nếu không tìm thấy tệp pom.xml, ta có thể giả định rằng thư mục target không phải là thư mục gốc của dự án
-            // Do đó, ta sẽ lấy thư mục cha của thư mục hiện tại
-            projectDirectory = new File(projectDirectory).getParent();
+        String target = FileUtils.getFile(classes).getParent();
+        System.out.println("target = " + target);
+
+        if (StringUtils.isNotEmpty(target)) {
+            target = new File(target).getParent();
+            System.out.println("Thư mục chứa project: " + target);
         }
-
-        System.out.println("Thư mục chứa project: " + projectDirectory);
     }
 
 }
