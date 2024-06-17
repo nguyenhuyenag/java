f45f47df8d18d8e7a4d229860cd11760adaddd70
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-06-17 17:15:45 +0700

    #

--- java-core/src/main/java/test/MainCore.java
+++ java-core/src/main/java/test/MainCore.java
@@ -3,7 +3,18 @@ package test;
 public class MainCore {
 
     public static void main(String[] args) throws Exception {
+        int[] nums = {1, 2, 3, 4};
+        int n = 3;
 
+        int maxValueOnNBits = 1 << n;
+
+        for (int i = 1; i <= maxValueOnNBits; i++) {
+            for (int bit = 0; bit < n; bit++) {
+                int mask = 1 << bit;
+                if ((i & mask) == mask) System.out.print(nums[bit]);
+            }
+            System.out.println();
+        }
     }
 
 }
