20b62a4788d23c58ba1a3bfef33bc1c209487640
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-06-25 09:03:52 +0700

    #

---
+++ java-core/src/main/java/jmh/BenchMarkTest.java
@@ -0,0 +1,54 @@
+package jmh;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+/**
+ * Based on https://github.com/tedyoung/indexof-contains-benchmark
+ */
+@Fork(1)
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class BenchMarkTest {
+
+    private final int M = 10_000;
+    private final int N = 10_000;
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+
+//    @Setup
+//    public void setup() {
+//    }
+
+    @Benchmark
+    public void test1() {
+        int[][] matrix = new int[M][N];
+        for (int i = 0; i < M * N; i++) {
+            int row = i / N;
+            int col = i % N;
+            matrix[row][col] = -1;
+        }
+    }
+
+    @Benchmark
+    public void test2() {
+        int[][] matrix = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+}
