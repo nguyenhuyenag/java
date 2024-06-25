335ce649425c9c53e6fdb43f6de23498d197149a
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-06-25 16:59:16 +0700

    #

--- java-core/src/main/java/test/MainCore.java
+++ java-core/src/main/java/test/MainCore.java
@@ -1,8 +1,26 @@
 package test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class MainCore {
 
     public static void main(String[] args) throws Exception {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            executor.execute(() -> {
+                System.out.println("Task đang được thực hiện bởi thread: " + Thread.currentThread().getName());
+            });
+        }
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
 
     }
 
