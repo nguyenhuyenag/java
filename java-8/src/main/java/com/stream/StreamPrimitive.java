10affda8c8bf07ca615f9874d8b1d94f361ed8cc
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-07-19 16:35:58 +0700

    #

--- java-8/src/main/java/com/stream/StreamPrimitive.java
+++ java-8/src/main/java/com/stream/StreamPrimitive.java
@@ -3,6 +3,7 @@ package com.stream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -12,12 +13,18 @@ public class StreamPrimitive {
 	// int[] -> IntStream
 	public static void convert1() {
 		IntStream intRange = IntStream.range(1, 5);
-		IntStream intStream = IntStream.of(new int[] { 1, 2, 3, 4, 5 });
+		IntStream intStream = IntStream.of(1, 2, 3, 4, 5);
 	}
 
 	// IntStream -> int[]
 	public static void convert2() {
-		int[] array = IntStream.range(1, 5).toArray();
+		// [1, 2, 3, 4]
+		int[] arr1 = IntStream.range(1, 5).toArray();
+		System.out.println(Arrays.toString(arr1));
+
+		// [1, 2, 3, 4, 5]
+		int[] arr2 = IntStream.rangeClosed(1, 5).toArray();
+		System.out.println(Arrays.toString(arr2));
 	}
 
 	// String -> IntStream
@@ -33,10 +40,14 @@ public class StreamPrimitive {
 
 	public void streamFromFile() {
 		try (Stream<String> lines = Files.lines(Paths.get(""))) {
-			lines.forEach(t -> System.out.println(t));
+			lines.forEach(System.out::println);
 		} catch (IOException e) {
-			
-		}
+            throw new RuntimeException(e);
+        }
+	}
+
+	public static void main(String[] args) {
+		convert2();
 	}
 
 }
