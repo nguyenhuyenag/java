b78b7b376918276e3624ea9c2427a110e9797aa5
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-08-15 21:51:23 +0700

    #

--- java-core/src/main/java/basic/strings/Escape.java
+++ java-core/src/main/java/basic/strings/Escape.java
@@ -5,8 +5,13 @@ import org.apache.commons.text.StringEscapeUtils;
 public class Escape {
 
     public static void main(String[] args) {
-        String incoming = "<html> <head></head> <body>  <p><span style=\"font-family: Arial;\">Ευχαριστώ (eff-kha-ri-STOE) Tι κανείς (tee-KAH-nis)? Mε συγχωρείτε.</span></p> </body></html>";
-        System.out.println(StringEscapeUtils.escapeJava(incoming));
-        System.out.println(StringEscapeUtils.escapeJson(incoming));
+        // String incoming = "<html> <head></head> <body>  <p><span style=\"font-family: Arial;\">Ευχαριστώ (eff-kha-ri-STOE) Tι κανείς (tee-KAH-nis)? Mε συγχωρείτε.</span></p> </body></html>";
+        String incoming = "{ \"message\" : \"Hello\", \"role\" : \"admin\" }";
+        String escaped = StringEscapeUtils.escapeJava(incoming);
+        System.out.println(escaped);
+        System.out.println(StringEscapeUtils.unescapeJava(escaped));
+        // System.out.println(StringEscapeUtils.escapeJson(incoming));
+        System.out.println(StringEscapeUtils.escapeHtml4(incoming));
+        System.out.println(StringEscapeUtils.escapeXml11(incoming));
     }
 }
