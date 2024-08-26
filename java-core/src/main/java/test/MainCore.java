ccce9e350e3c688ceca6fe60e298d56f7f8aaab5
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-08-26 17:16:07 +0700

    #

--- java-core/src/main/java/test/MainCore.java
+++ java-core/src/main/java/test/MainCore.java
@@ -1,27 +1,16 @@
 package test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class MainCore {
 
-    public static void main(String[] args) throws Exception {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
-        for (int i = 0; i < 5; i++) {
-            executor.execute(() -> {
-                System.out.println("Task đang được thực hiện bởi thread: " + Thread.currentThread().getName());
-            });
-        }
-        executor.shutdown();
-        try {
-            if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
-                executor.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            executor.shutdownNow();
-        }
-
+    public static String stringMerge(String s1, String s2, char letter) {
+        return null;
     }
+
+//    @org.junit.jupiter.api.Test
+//    public void test() {
+//        Assertions.assertEquals();
+//        Assertions.assertArrayEquals();
+//        Assertions.assertIterableEquals();
+//    }
 
 }
