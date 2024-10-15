6dcc31d626c84b24cd05cba09c46916cf5dc7e17
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-10-15 13:49:28 +0700

    #

--- java-core/src/main/java/common/util/lzma/LZMASample.java
+++ java-core/src/main/java/common/util/lzma/LZMASample.java
@@ -11,6 +11,8 @@ import java.nio.file.Paths;
 
 /*
     Cần 2 thư viện: commons-compress, xz
+
+    https://www.baeldung.com/apache-commons-compress-project
  */
 public class LZMASample {
 
@@ -35,13 +37,13 @@ public class LZMASample {
     // Method to decompress an LZMA file back to its original PDF format
     public static File decompressLzma(String inputLzmaPath, String outputPdfPath) {
         File decompressedFile = new File(outputPdfPath);
-        try (InputStream fileInputStream = new FileInputStream(inputLzmaPath);
-             LZMACompressorInputStream lzmaInputStream = new LZMACompressorInputStream(fileInputStream);
-             OutputStream fileOutputStream = new FileOutputStream(decompressedFile)) {
+        try (InputStream fis = new FileInputStream(inputLzmaPath);
+             LZMACompressorInputStream lzmaInputStream = new LZMACompressorInputStream(fis);
+             OutputStream fos = new FileOutputStream(decompressedFile)) {
             byte[] buffer = new byte[1024];
             int len;
             while ((len = lzmaInputStream.read(buffer)) != -1) {
-                fileOutputStream.write(buffer, 0, len);
+                fos.write(buffer, 0, len);
             }
             System.out.println("Decompression completed!");
         } catch (IOException e) {
@@ -50,13 +52,34 @@ public class LZMASample {
         return decompressedFile;
     }
 
+    // Method to compress a byte array using LZMA
+    public static byte[] compressByteArray(byte[] inputData) throws IOException {
+        // Create an input stream from the byte array
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(inputData);
+
+        // Create an output stream to hold the compressed data
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        // Wrap the output stream with LZMACompressorOutputStream to compress data
+        try (LZMACompressorOutputStream lzmaOutputStream = new LZMACompressorOutputStream(byteArrayOutputStream)) {
+            byte[] buffer = new byte[1024]; // Buffer for reading the input data in chunks
+            int len;
+            while ((len = byteArrayInputStream.read(buffer)) != -1) {
+                lzmaOutputStream.write(buffer, 0, len);
+            }
+        }
+
+        // Get the compressed data from the output stream
+        return byteArrayOutputStream.toByteArray();
+    }
+
     public static void main(String[] args) throws IOException {
         String path = "C:/Users/huyennv/Desktop/ts24pro_mobile";
 
         String inputFile = path + "/0309478306999-333-HDLD.pdf";
 
-        String compressedLzma = path + "/compressed-file.lzma";
         String format = FileNameUtils.getExtension(inputFile);
+        String compressedLzma = path + "/compressed-file-" + format + ".lzma";
         String decompressedLzma = path + "/decompressed-file." + format;
 
         // TODO
