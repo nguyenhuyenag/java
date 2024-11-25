c475cfd2a923a422fcb9665b0d97ef88463ebae4
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-11-25 10:35:13 +0700

    #

--- java-core/src/main/java/trick/InitValueInIfCondition.java
+++ java-core/src/main/java/trick/InitValueInIfCondition.java
@@ -1,19 +1,18 @@
 package trick;
 
 public class InitValueInIfCondition {
-	
-	public static boolean init(final CharSequence cs) {
+
+    public static void init(final CharSequence cs) {
         int strLen;
         if (cs == null || (strLen = cs.length()) == 0) { // <- here
-            return true;
+            return;
         }
         System.out.println("Length: " + strLen);
-        return true;
     }
-	
-	public static void main(String[] args) {
-		String input = "Java123";
-		init(input);
-	}
-	
+
+    public static void main(String[] args) {
+        String input = "Java123";
+        init(input);
+    }
+
 }
