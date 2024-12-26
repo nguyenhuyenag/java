b1e2d2a2bf5ba950bc817279b51308874817aeb4
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-12-26 21:52:18 +0700

    #

--- java-8/src/main/java/com/iterface/fi/FunctionInterface.java
+++ java-8/src/main/java/com/iterface/fi/FunctionInterface.java
@@ -1,44 +1,48 @@
 package com.iterface.fi;
 
-@FunctionalInterface
-interface StringUtils {
-	int process(String s);
-	// int process2(String s); // Đóng/mở để thấy sự khác biệt
-}
-
-/**
- * Functional Interface là một interface chỉ có duy nhất một phương thức trừu
- * tượng (abstract method ~ abstract function) hoặc single abstract method
- * (SAM). Ví dụ Predicate, Function, Runnable, Callable,...
+/*-
+    Functional Interface là một interface chỉ có duy nhất một phương thức trừu
+    tượng (abstract method ~ abstract function) hoặc single abstract method
+    (SAM). Ví dụ Predicate, Function, Runnable, Callable,...
  */
 public class FunctionInterface {
 
-	public static int doAction(String input, StringUtils processor) {
-		return processor.process(input);
-	}
+    @FunctionalInterface
+    interface MyStringUtils {
 
-	public static int withoutLambda() { // <- anonymous function
-		return doAction("Hello Java", new StringUtils() {
-			@Override
-			public int process(String s) {
-				return s.length();
-			}
-		});
-	}
+        // Hàm sẽ thực hiện công việc gì đó với chuỗi truyền vào
+        int process(String s);
 
-	public static int useLambda() {
-		return doAction("Use lambda", s -> s.length());
-	}
+        // int process2(String s); // Đóng/mở để thấy sự khác biệt
 
-	public static int methodReference() {
-		return doAction("Method reference", String::length);
-	}
+    }
 
-	public static void main(String[] args) {
-		int length = useLambda();
-		// int length = withoutLambda();
-		// int length = methodReference();
-		System.out.println(length);
-	}
+    public static int doAction(String input, MyStringUtils processor) {
+        return processor.process(input);
+    }
+
+    public static int withoutLambda() { // <- anonymous function
+        return doAction("Hello Java", new MyStringUtils() {
+            @Override
+            public int process(String s) {
+                return s.length();
+            }
+        });
+    }
+
+    public static int useLambda() {
+        return doAction("Use lambda", s -> s.length());
+    }
+
+    public static int methodReference() {
+        return doAction("Method reference", String::length);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("useLambda(): " + useLambda());
+        System.out.println("withoutLambda(): " + withoutLambda());
+        System.out.println("methodReference(): " + methodReference());
+
+    }
 
 }
--- java-8/src/main/java/com/iterface/fp/FunctionalProgramming.java
+++ java-8/src/main/java/com/iterface/fp/FunctionalProgramming.java
@@ -1,32 +1,37 @@
 package com.iterface.fp;
 
-/**
- * - Lập trình hướng hàm là đưa hành vi vào hàm, mục đích của nó là đưa hành vi
- * vào hàm. Hay nói cách khác là đưa thêm các đoạn code vào hàm như là một
- * parameter.
+/*-
+ 	Lập trình hướng hàm là đưa hành vi vào hàm, mục đích của nó là đưa hành vi vào
+ 	hàm. Hay nói cách khác là đưa thêm các đoạn code vào hàm như là một parameter.
  */
 public class FunctionalProgramming {
 
-	@FunctionalInterface
-	interface NumberFunction {
-		int process(int a, int b);
-	}
+    @FunctionalInterface
+    interface NumberFunction {
 
-	public static int doAction(int a, int b, NumberFunction function) {
-		return function.process(a, b);
-	}
+        // Hàm sẽ thực hiện công việc gì đó với 2 số được truyền vào
+        int process(int a, int b);
 
-	public static void main(String[] args) {
-		int a = 3, b = 4;
-		
-		int sum = doAction(a, b, (x, y) -> x + y);
-		System.out.println(a + " + " + b + " = " + sum);
+    }
 
-		int product = doAction(a, b, (x, y) -> x * y);
-		System.out.println(a + " * " + b + " = " + product);
+    public static int doAction(int a, int b, NumberFunction function) {
+        return function.process(a, b);
+    }
 
-		int max = doAction(a, b, Math::max);
-		System.out.println("max(" + a + ", " + b + ") = " + max);
-	}
+    public static void main(String[] args) {
+        int a = 3, b = 4;
+
+        // Cộng 2 số
+        int sum = doAction(a, b, (x, y) -> x + y);
+        System.out.println(a + " + " + b + " = " + sum);
+
+        // Nhân 2 số
+        int product = doAction(a, b, (x, y) -> x * y);
+        System.out.println(a + " * " + b + " = " + product);
+
+        // Tìm max(a,b)
+        int max = doAction(a, b, Math::max);
+        System.out.println("max(" + a + ", " + b + ") = " + max);
+    }
 
 }
