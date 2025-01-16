c9acabc016d7cba4493a6ae8d21d7881e260b777
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-01-16 08:35:57 +0700

    #

--- libraries/qr-code/src/main/java/com/qr/QRCode.java
+++ libraries/qr-code/src/main/java/com/qr/QRCode.java
@@ -1,9 +1,12 @@
 package com.qr;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 
+import com.google.zxing.MultiFormatWriter;
 import org.apache.commons.lang3.SystemUtils;
 
 import com.google.zxing.BarcodeFormat;
@@ -14,19 +17,34 @@ import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QRCode {
 
-	public static void main(String[] args) throws WriterException, IOException {
-	    String data = "https://translate.google.com";
+    static int width = 400;
+    static int height = 400;
+    private static final String extension = "png";
 
-	    int width = 400;
-		int height = 400;
-		QRCodeWriter qrCodeWriter = new QRCodeWriter();
-	    BitMatrix matrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+    public static void toQRBase64() throws WriterException, IOException {
+        String url = "https://news.bbc.co.uk";
+        BitMatrix matrix = new MultiFormatWriter()
+                .encode(url, BarcodeFormat.QR_CODE, width, height);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(matrix, extension, bos);
+        String image = Base64.getEncoder().encodeToString(bos.toByteArray()); // base64 encode
+        System.out.println("Base64: " + image);
+    }
 
-	    // Write to file imaged
-	    Path path = Paths.get(SystemUtils.USER_DIR, "output", "qr_code.png");
-	    MatrixToImageWriter.writeToPath(matrix, "PNG", path);
-	    
-	    System.out.println("OK");
-	}
+
+    public static void toQRImage() throws WriterException, IOException {
+        String data = "https://translate.google.com";
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix matrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+        // Write to file imaged
+        Path path = Paths.get(SystemUtils.USER_DIR, "output", "qr_code.png");
+        MatrixToImageWriter.writeToPath(matrix, extension, path);
+        System.out.println("OK");
+    }
+
+    public static void main(String[] args) throws IOException, WriterException {
+        // toQRImage();
+        toQRBase64();
+    }
 
 }
