3f6093a1f0336e806630b9d7ac516863e28311fe
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-01-16 08:38:14 +0700

    #

--- libraries/qr-code/src/main/java/com/qr/QRCode.java
+++ libraries/qr-code/src/main/java/com/qr/QRCode.java
@@ -21,17 +21,7 @@ public class QRCode {
     static int height = 400;
     private static final String extension = "png";
 
-    public static void toQRBase64() throws WriterException, IOException {
-        String url = "https://news.bbc.co.uk";
-        BitMatrix matrix = new MultiFormatWriter()
-                .encode(url, BarcodeFormat.QR_CODE, width, height);
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        MatrixToImageWriter.writeToStream(matrix, extension, bos);
-        String image = Base64.getEncoder().encodeToString(bos.toByteArray()); // base64 encode
-        System.out.println("Base64: " + image);
-    }
-
-
+    // Using QRCodeWriter()
     public static void toQRImage() throws WriterException, IOException {
         String data = "https://translate.google.com";
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
@@ -40,6 +30,16 @@ public class QRCode {
         Path path = Paths.get(SystemUtils.USER_DIR, "output", "qr_code.png");
         MatrixToImageWriter.writeToPath(matrix, extension, path);
         System.out.println("OK");
+    }
+
+    // Using QRCodeWriter()
+    public static void toQRBase64() throws WriterException, IOException {
+        String url = "https://news.bbc.co.uk";
+        BitMatrix matrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(matrix, extension, bos);
+        String image = Base64.getEncoder().encodeToString(bos.toByteArray()); // base64 encode
+        System.out.println("Base64: " + image);
     }
 
     public static void main(String[] args) throws IOException, WriterException {
