9bd1be33675c03e2eef72f5f469582fd4af7fa81
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-05-03 12:22:51 +0700

    #

--- java-core/src/main/java/test/MainCore.java
+++ java-core/src/main/java/test/MainCore.java
@@ -1,9 +1,13 @@
 package test;
 
+import com.fasterxml.uuid.Generators;
+
 public class MainCore {
 
     public static void main(String[] args) {
-
+        System.out.println("UUID Version 1: " + Generators.timeBasedGenerator().generate());
+        System.out.println("UUID Version 6: " + Generators.timeBasedReorderedGenerator().generate());
+        System.out.println("UUID Version 7: " + Generators.timeBasedEpochGenerator().generate());
     }
 
 }
